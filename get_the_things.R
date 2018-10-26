30d281898fa47e53f44bb6f151c2920512daecac
kiadso <genelle@eyedarts.net>
2018-10-26 16:55:06 -0400

    Assessments is DONE!! Got it into a giant tiny table.

--- get_the_things.R
+++ get_the_things.R
@@ -318,7 +318,6 @@ entry_exits <- entry_exits %>% mutate(
 cols <- c(
   "reviewDate",
   "reviewType",
-  "active",
   "review_id",
   "ee_id"
 )
@@ -328,12 +327,23 @@ interims <- xml_to_df(y, "//records/entryExitRecords/EntryExit[active = 'true']/
 interims$review_id <- parse_number(xml_text(xml_find_all(y, "//records/entryExitRecords/EntryExit[active = 'true']/childEntryExitReview/EntryExitReview/@system_id")))
 # establishing the node we're counting up from
 interim_node <- xml_find_all(y, "//records/entryExitRecords/EntryExit[active = 'true']/childEntryExitReview/EntryExitReview")
-# going up the tree from there, stopping at ee's
+
 ee_as_gparent <- xml_parent(xml_parent(interim_node))
 # grabbing the attribute that's at those particular ee's
-interims$ee_id <- parse_number(xml_attr(ee_as_gparent, "record_id"))
+
+length_interim <- sapply(xml_parent(interim_node), function(x) length(xml_children(x)))
+
+ee_id <- parse_number(xml_attr(ee_as_gparent, "record_id"))
+
+ids <- data.frame(length_interim, ee_id)
+
+ee_ids <- c()
+for(i in 1:nrow(ids)) {
+  ee_ids <- c(ee_ids, rep(ids[i,]$ee_id, ids[i,]$length_interim))
+}
+interims$ee_id <- ee_ids
 # clean up the house
-rm(interim_node, ee_as_gparent)
+rm(interim_node, ee_as_gparent, length_interim, ee_id, ids, ee_ids)
 
 # Client Records ----------------------------------------------------------
 # name nodes we want to pull in
@@ -385,80 +395,118 @@ colnames(clients) <- c(
 # Assessment Records ------------------------------------------------------
 # THE PLAN WITH ALL THIS IS GETTING ONE GIANT TINY TABLE
 cols <- c(
-  "record_id",
+  "client_id",
   "data_element",
   "value",
   "date_added",
   "date_effective"
 )
-# 
-x <- xml_find_all(y, xpath = "//records/clientRecords/Client/assessmentData")
-client_id <- xml_attr(xml_parent(x), "record_id")
-
-#this returns all names of each node under assessmentData, 
-#including subassessment nodes which need to be removed
-xml_name(xml_children(x))
-
-
-# THE PLAN WITH ALL THIS IS BREAKING EACH DE INTO A SEPARATE TABLE
-# name nodes we want to pull in
-cols <- c(
-  "record_id",
-  "hud_housingmoveindate",
-  "date_added",
-  "date_effective"
-)
-
-#returning 0 records
-move_in_date <- xml_to_df(y, "//records/clientRecords/Client/assessmentData/hud_housingmoveindate", cols)
-
-move_in_date_nodes <-
-  xml_find_all(y, xpath = "//records/clientRecords/Client/assessmentData/hud_housingmoveindate")
-df <- as.data.frame(setNames(replicate(length(cols), character(0), simplify = F), cols))
-dates <- as.data.frame(xml_text(move_in_date_nodes), col.names = "hud_housingmoveindate")
-#getting fewer client ids than i am move in dates, probably because a client can have more than 1
-#adding an extra column with a garbage name
-df <- bind_rows(df, dates)
-
-client_as_gparent <- xml_parent(xml_parent(move_in_date_nodes))
-#move_in_date$hud_housingmoveindate <- xml_text(move_in_date_nodes)
-move_in_date$record_id <- parse_number(xml_attr(client_as_gparent, "record_id"))
-move_in_date$date_added <- xml_text(xml_find_all(y, "//records/clientRecords/Client/hud_housingmoveindate/@date_added"))
-move_in_date$date_effective <- xml_text(xml_find_all(y, "//records/clientRecords/Client/hud_housingmoveindate/@date_effective"))
-
-# THE IDEA HERE IS JUST WALKING THROUGH THE OLD FOR LOOP, TIMING THINGS
-
-df <- as.data.frame(setNames(replicate(length(cols), character(0), simplify = F), cols))
-i <- 1
-for(child in records){
-  child_ = xml_children(child) #this is too slow
-  assessment <- child_[length(child_)] # this is also slow
-  df2 <- as.data.frame(setNames(replicate(length(cols), character(0), simplify = F), cols))
-  idx <- c()
-  effective_dates <- xml_text(xml_find_all(xml_children(assessment), "@date_effective"))
-  for(child2 in assessment) {
-    child__ <- xml_children(child2)
-    data = xml_text(child__)
-    names(data) <- xml_name(child__)
-    idx <- which(names(data) %in% cols)
-    data = data[idx]
-    effective_dates <- effective_dates[idx]
-    for(i in 1:length(data)) {
-      names(effective_dates)[i] <- paste0(names(data)[i], ".effective")
-    }
-    data <- c(data, effective_dates)
-    df2 <- bind_rows(df2, data.frame(as.list(data)))
-  }
-  df2$record_id = rep(ids[i], nrow(df2))
-  df <- bind_rows(df, df2)
-  print(i)
-  i <- i + 1
+# 2742036 nodes (assessment records, including subs)
+assessmentData_child_nodes <- xml_find_all(y, xpath = "//records/clientRecords/Client/assessmentData/*")
+# 42754 nodes (client records)
+Client_ID_as_gparent <- xml_parent(xml_parent(assessmentData_child_nodes))
+# 2742036 records
+data_element <- xml_name(assessmentData_child_nodes)
+# 2742036 records
+value <- xml_text(assessmentData_child_nodes)
+# 2742036 records
+date_effective <- xml_attr(assessmentData_child_nodes, "date_effective")
+# 2742036 records
+date_added <- xml_attr(assessmentData_child_nodes, "date_added")
+# 42754 records
+client_id <- parse_number(xml_attr(Client_ID_as_gparent, "record_id"))
+# 42754 records
+length_assessments <- sapply(xml_parent(assessmentData_child_nodes), function(x) length(xml_children(x)))
+# 42754 records
+ids <- data.frame(length_assessments, client_id)
+# getting the 42754 records onto the larger number of rows
+a <- c()
+for(i in 1:nrow(ids)) {
+  a <- c(a, rep(ids[i,]$client_id, ids[i,]$length_assessments))
 }
-assessment_data <- df
-# clean up column names
-
-# clean up data to match with HUD CSV specs and make id field numeric
-z <- xml_find_all(y, xpath = "//records/clientRecords/Client/assessmentData")
+# and thank you
+client_id <- a
+# putting it all together
+assessment_data <- bind_cols(list(client_id, data_element, value, date_effective, date_added))
+# naming the columns
+colnames(assessment_data) <- c(
+  "client_id" = "Client_ID",
+  "data_element" = "Data_Element",
+  "value" = "Value",
+  "date_effective" =  "Date_Effective",
+  "date_added" = "Date_Added"
+)
+# clean the house
+rm(assessmentData_child_nodes, 
+   Client_ID_as_gparent, 
+   data_element, 
+   value, 
+   date_effective, 
+   date_added, 
+   client_id, 
+   a,
+   i,
+   length_assessments, 
+   ids)
+# # THE PLAN WITH ALL THIS IS BREAKING EACH DE INTO A SEPARATE TABLE
+# # name nodes we want to pull in
+# cols <- c(
+#   "record_id",
+#   "hud_housingmoveindate",
+#   "date_added",
+#   "date_effective"
+# )
+# 
+# #returning 0 records
+# move_in_date <- xml_to_df(y, "//records/clientRecords/Client/assessmentData/hud_housingmoveindate", cols)
+# 
+# move_in_date_nodes <-
+#   xml_find_all(y, xpath = "//records/clientRecords/Client/assessmentData/hud_housingmoveindate")
+# df <- as.data.frame(setNames(replicate(length(cols), character(0), simplify = F), cols))
+# dates <- as.data.frame(xml_text(move_in_date_nodes), col.names = "hud_housingmoveindate")
+# #getting fewer client ids than i am move in dates, probably because a client can have more than 1
+# #adding an extra column with a garbage name
+# df <- bind_rows(df, dates)
+# 
+# client_as_gparent <- xml_parent(xml_parent(move_in_date_nodes))
+# #move_in_date$hud_housingmoveindate <- xml_text(move_in_date_nodes)
+# move_in_date$record_id <- parse_number(xml_attr(client_as_gparent, "record_id"))
+# move_in_date$date_added <- xml_text(xml_find_all(y, "//records/clientRecords/Client/hud_housingmoveindate/@date_added"))
+# move_in_date$date_effective <- xml_text(xml_find_all(y, "//records/clientRecords/Client/hud_housingmoveindate/@date_effective"))
+# 
+# # THE IDEA HERE IS JUST WALKING THROUGH THE OLD FOR LOOP, TIMING THINGS
+# 
+# df <- as.data.frame(setNames(replicate(length(cols), character(0), simplify = F), cols))
+# i <- 1
+# for(child in records){
+#   child_ = xml_children(child) #this is too slow
+#   assessment <- child_[length(child_)] # this is also slow
+#   df2 <- as.data.frame(setNames(replicate(length(cols), character(0), simplify = F), cols))
+#   idx <- c()
+#   effective_dates <- xml_text(xml_find_all(xml_children(assessment), "@date_effective"))
+#   for(child2 in assessment) {
+#     child__ <- xml_children(child2)
+#     data = xml_text(child__)
+#     names(data) <- xml_name(child__)
+#     idx <- which(names(data) %in% cols)
+#     data = data[idx]
+#     effective_dates <- effective_dates[idx]
+#     for(i in 1:length(data)) {
+#       names(effective_dates)[i] <- paste0(names(data)[i], ".effective")
+#     }
+#     data <- c(data, effective_dates)
+#     df2 <- bind_rows(df2, data.frame(as.list(data)))
+#   }
+#   df2$record_id = rep(ids[i], nrow(df2))
+#   df <- bind_rows(df, df2)
+#   print(i)
+#   i <- i + 1
+# }
+# assessment_data <- df
+# # clean up column names
+# 
+# # clean up data to match with HUD CSV specs and make id field numeric
+# z <- xml_find_all(y, xpath = "//records/clientRecords/Client/assessmentData")
 
 # Needs ------------------------------------------------------------
 # name nodes we want to pull in
