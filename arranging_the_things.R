1be629c8d8fc8b4ddebec13f31e678b82931d6a9
kiadso <genelle@eyedarts.net>
2018-11-13 14:09:56 -0500

    figured out how to get client-level assessment data from the assessment_data. wasn't able to get the process into a function yet, but I feel like I'm close!

--- arranging_the_things.R
+++ arranging_the_things.R
@@ -5,24 +5,37 @@ w <- read_csv("C:\\Users\\laptop\\Documents\\sampledates.csv")
 library("janitor")
 race2 %>% get_dupes(PersonalID)
 
+
+# Function to get most recent value for CLIENT-LEVEL assessment data ------
+client_level_value <- function(dataelement) {
+  x <- dataelement %>% group_by(PersonalID) %>% summarise(max(DateEffective), max(DateAdded))
+  dataelement <- semi_join(dataelement, x, by = c("PersonalID", "DateAdded" = "max(DateAdded)", "DateEffective" = "max(DateEffective)"))
+}
+# this ^^ does not work when used as a function, but it does work if run by itself (see below)
+
+race2 <- assessment_data %>% filter(DataElement == "svpprofsecondaryrace")
+summarised_race2 <- race2 %>% group_by(PersonalID) %>% summarise(max(DateEffective), max(DateAdded)) 
+race2 <- semi_join(race2, summarised_race2, by = c("PersonalID", "DateAdded" = "max(DateAdded)", "DateEffective" = "max(DateEffective)"))
+
 # ONE answer per CLIENt ---------------------------------------------------
 dob <- assessment_data %>% filter(DataElement == "svpprofdob")
 dob_dq <- assessment_data %>% filter(DataElement == "svpprofdobtype")
 race1 <- assessment_data %>% filter(DataElement == "svpprofrace")
-race2 <- assessment_data %>% filter(DataElement == "svpprofsecondaryrace")
-summarised_race2 <- race2 %>% group_by(PersonalID) %>% summarise(max(DateAdded))
+
+
+
+
 ethnicity <- assessment_data %>% filter(DataElement == "svpprofeth")
-disabling_condition <- assessment_data %>% filter (DataElement == "")
+disabling_condition <- assessment_data %>% filter (DataElement == "hud_disablingcondition")
 # add all the client-level data elements into the Client table
 
 # ONE answer per ENROLLMENT -----------------------------------------------
-county_served <- assessment_data %>% filter(DataElement == "")
-CoC_served <- assessment_data %>% filter(DataElement == "")
-residence_prior <- assessment_data %>% filter(DataElement == "")
-length_of_time <- assessment_data %>% filter(DataElement == "")
-county_prior <- assessment_data %>% filter(DataElement == "")
-LH_prior <- assessment_data %>% filter(DataElement == "")
-Approx_date_homeless <- assessment_data %>% filter(DataElement == "")
-DV_yesno <- assessmenet_data %>% filter(DataElement == "")
-DV_whenx <- assessment_data %>% filter(DataElement == "")
-DV_fleeing <- assessment_data %>% filter(DataElement == "")
+CoC_served <- assessment_data %>% filter(DataElement == "hud_cocclientlocation")
+residence_prior <- assessment_data %>% filter(DataElement == "typeoflivingsituation")
+length_of_time <- assessment_data %>% filter(DataElement == "hud_lengthofstay")
+LH_prior_90days <- assessment_data %>% filter(DataElement == "hud_lengthstay_less90days")
+LH_prior_7days <- assessment_data %>% filter(DataElement == "hud_lengthstay_less7nights")
+Approx_date_homeless <- assessment_data %>% filter(DataElement == "hud_homelessstartdate")
+DV_yesno <- assessmenet_data %>% filter(DataElement == "domesticviolencevictim")
+DV_whenx <- assessment_data %>% filter(DataElement == "hud_extentofdv")
+DV_fleeing <- assessment_data %>% filter(DataElement == "hud_extentofdv2")
