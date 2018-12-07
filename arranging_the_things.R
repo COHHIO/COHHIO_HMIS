658a669e24c234a4dd13513b62fa7c38082570f6
kiadso <genelle@eyedarts.net>
2018-12-07 16:01:19 -0500

    added some logic to the Client table function, created a function for the Enrollment table's assessment data

--- arranging_the_things.R
+++ arranging_the_things.R
@@ -1,150 +1,118 @@
 # ONE answer per CLIENt ---------------------------------------------------
 client_level_value <- function(dataelement) {
-  x <- dataelement %>% 
-    group_by(PersonalID) %>% 
+  relevant_data <- filter(assessment_data, DataElement == dataelement)
+  no_dupes <- relevant_data %>%
+    group_by(PersonalID) %>%
     summarise(max(DateEffective), max(DateAdded))
-  dataelement <- semi_join(dataelement, x, 
-                           by = c("PersonalID", 
-                                  "DateAdded" = "max(DateAdded)", 
-                                  "DateEffective" = "max(DateEffective)"))
-  return(dataelement)
+  x <- semi_join(relevant_data, no_dupes,
+                 by = c("PersonalID", "DateAdded" = "max(DateAdded)",
+                        "DateEffective" = "max(DateEffective)"))
+  Client <- left_join(Client, relevant_data, by = "PersonalID") %>%
+    select(-DataElement, -DateEffective, -DateAdded) 
+  return(Client)
 }
-dob <- assessment_data %>% filter(DataElement == "svpprofdob")
-dob <- client_level_value(dob)
-dob <- mutate(dob, 
-              DOB = strftime(ymd_hms(Value), "%F"),
-              Value = NULL,
-              DataElement = NULL,
-              DateEffective = NULL,
-              DateAdded = NULL)
-dob_dq <- assessment_data %>% filter(DataElement == "svpprofdobtype")
-dob_dq <- client_level_value(dob_dq)
-dob_dq <- dob_dq %>% mutate(
-  DOBDataQuality = Value,
-  DataElement = NULL,
-  DateEffective = NULL,
-  DateAdded = NULL,
-  Value = NULL
-)
+Client <- client_level_value("svpprofdob") 
+Client <- Client %>% mutate(DOB = strftime(ymd_hms(Value), "%F"), Value = NULL)
+Client <- client_level_value("svpprofdobtype") %>% rename(DOBDataQuality = Value)
+Client <- client_level_value("svpprofeth") %>% rename(Ethnicity = Value)
+Client <- client_level_value("hud_disablingcondition") %>% rename(DisablingCondition = Value)
+
 race <-
   assessment_data %>% filter(
     DataElement %in% c("svpprofrace", "svpprofsecondaryrace") &
     Value != "" &
       (DataElement == "svpprofrace" |
     (DataElement == "svpprofsecondaryrace" &
-        !(Value %in% c(
-          "client doesn't know (hud)",
-          "client refused (hud)",
-          "data not collected (hud)"))
+        !(Value %in% c("client doesn't know (hud)", "client refused (hud)", "data not collected (hud)"))
     ))
   ) 
 x <- race %>% group_by(PersonalID, Value) %>%  # eliminates the two identical races saved problem
   summarise(max(DateEffective), max(DateAdded)) # only keeps the most recent record
-race <- mutate(
-  x,
+race <- mutate(x,
   AmIndAKNative = if_else(Value == "american indian or alaska native (hud)", 1, 0),
   Asian = if_else(Value == "asian (hud)", 1, 0),
   BlackAfAmerican = if_else(Value == "black or african american (hud)", 1, 0),
   NativeHIOtherPacific = if_else(Value == "native hawaiian or other pacific islander (hud)", 1, 0),
   White = if_else(Value == "white (hud)", 1, 0),
-  DataElement = NULL,
-  Value = NULL,
-  DateEffective = NULL,
-  DateAdded = NULL
+  DataElement = NULL, Value = NULL, DateEffective = NULL, DateAdded = NULL)
+race <- race %>% group_by(PersonalID) %>%
+  summarise(
+    AmIndAKNative = sum(AmIndAKNative),
+    Asian = sum(Asian),
+    BlackAfAmerican = sum(BlackAfAmerican),
+    NativeHIOtherPacific = sum(NativeHIOtherPacific),
+    White = sum(White)
   )
-race <- race %>% group_by(PersonalID) %>% 
-  summarise(AmIndAKNative = sum(AmIndAKNative),
-            Asian = sum(Asian),
-            BlackAfAmerican = sum(BlackAfAmerican),
-            NativeHIOtherPacific = sum(NativeHIOtherPacific),
-            White = sum(White))
-ethnicity <- assessment_data %>% filter(DataElement == "svpprofeth")
-ethnicity <- client_level_value(ethnicity)
-ethnicity <- ethnicity %>% mutate(
-  DataElement = NULL,
-  Ethnicity = Value,
-  DateEffective = NULL,
-  DateAdded = NULL,
-  Value = NULL
-)
-disabling_condition <- assessment_data %>% filter (DataElement == "hud_disablingcondition")
-disabling_condition <- client_level_value(disabling_condition)
-disabling_condition <- disabling_condition %>% mutate(
-  DisablingCondition = Value,
-  DataElement = NULL,
-  Value = NULL,
-  DateEffective = NULL,
-  DateAdded = NULL
-) #check to see if this is actually leaving you with one row per client 
-# add all the client-level data elements into the Client table
-Client <- left_join(Client, dob, by = "PersonalID")
-Client <- left_join(Client, dob_dq, by = "PersonalID")
-Client <- left_join(Client, race, by = "PersonalID")
-Client <- left_join(Client, ethnicity, by = "PersonalID")
-Client <- left_join(Client, disabling_condition, by = "PersonalID")
+
 # tidy up the Client table
-Client <- mutate(Client,
-                 DOBDataQuality = case_when(
-                   DOBDataQuality == "full dob reported (hud)" ~ 1,
-                   DOBDataQuality == "approximate or partial dob reported (hud)" ~ 2,
-                   DOBDataQuality == "client doesn't know (hud)" ~ 8,
-                   DOBDataQuality == "client refused (hud)" ~ 9,
-                   is.na(DOBDataQuality) | DOBDataQuality == "data not collected (hud)" ~ 99
-                 ),
-                 Ethnicity = case_when(
-                   Ethnicity == "non-hispanic/non-latino (hud)" ~ 0,
-                   Ethnicity == "hispanic/latino (hud)" ~ 1,
-                   Ethnicity == "client doesn't know (hud)" ~ 8,
-                   Ethnicity == "client refused (hud)" ~ 9,
-                   is.na(Ethnicity) | Ethnicity == "" | Ethnicity == "data not collected (hud)" ~ 99
-                 ),
-                 DisablingCondition = case_when(
-                   DisablingCondition == "yes (hud)" ~ 1,
-                   DisablingCondition == "no (hud)" ~ 2,
-                   DisablingCondition == "client doesn't know (hud)" ~ 8,
-                   DisablingCondition == "client refused (hud)" ~ 9,
-                   DisablingCondition == "data not collected (hud)" | is.na(DisablingCondition) ~ 99
-                 ),
-                 RaceNone = if_else(AmIndAKNative + 
-                                      Asian + 
-                                      BlackAfAmerican + 
-                                      NativeHIOtherPacific + 
-                                      White == 0, 1, 0)
-                 )
-rm(disabling_condition, dob, dob_dq, ethnicity, race, x)
+Client <- mutate(
+  Client,
+  DOBDataQuality = case_when(
+    DOBDataQuality == "full dob reported (hud)" ~ 1,
+    DOBDataQuality == "approximate or partial dob reported (hud)" ~ 2,
+    DOBDataQuality == "client doesn't know (hud)" ~ 8,
+    DOBDataQuality == "client refused (hud)" ~ 9,
+    is.na(DOBDataQuality) |
+      DOBDataQuality == "data not collected (hud)" ~ 99
+  ),
+  Ethnicity = case_when(
+    Ethnicity == "non-hispanic/non-latino (hud)" ~ 0,
+    Ethnicity == "hispanic/latino (hud)" ~ 1,
+    Ethnicity == "client doesn't know (hud)" ~ 8,
+    Ethnicity == "client refused (hud)" ~ 9,
+    is.na(Ethnicity) |
+      Ethnicity == "" | Ethnicity == "data not collected (hud)" ~ 99
+  ),
+  DisablingCondition = case_when(
+    DisablingCondition == "yes (hud)" ~ 1,
+    DisablingCondition == "no (hud)" ~ 2,
+    DisablingCondition == "client doesn't know (hud)" ~ 8,
+    DisablingCondition == "client refused (hud)" ~ 9,
+    DisablingCondition == "data not collected (hud)" |
+      is.na(DisablingCondition) ~ 99
+  ),
+  RaceNone = if_else(
+    AmIndAKNative +
+      Asian +
+      BlackAfAmerican +
+      NativeHIOtherPacific +
+      White == 0,
+    1,
+    0
+  ),
+  DOB = ymd(DOB)
+)
+rm(race, x)
 # ONE answer per ENROLLMENT -----------------------------------------------
 setDT(assessment_data)
 small_enrollment <- Enrollment %>% select(EnrollmentID, PersonalID, EntryDate, ExitDate)
 small_enrollment <- setDT(small_enrollment)
-# Relationship to HoH
-ReltoHoH <-
-  assessment_data %>% filter(DataElement == "hud_relationtohoh") 
-tmp <- ReltoHoH %>%
-  group_by(PersonalID, DateEffective) %>%
-  summarise(DateAdded = max(DateAdded))
-ReltoHoH <- semi_join(ReltoHoH, tmp, by = c("PersonalID", "DateAdded"))
-x <- left_join(small_enrollment, ReltoHoH, by = "PersonalID") %>%
-  mutate(
-    DateEffective = ymd_hms(DateEffective),
-    EntryDate = ymd_hms(EntryDate),
-    ExitDate = ymd_hms(ExitDate))
+# function to pull assessment data into Enrollment table
+enrollment_level_value <- function(dataelement) {
+  relevant_data <- assessment_data %>% filter(DataElement == dataelement)
+  no_dupes <- relevant_data %>%
+    group_by(PersonalID, DateEffective) %>%
+    summarise(DateAdded = max(DateAdded))
+  relevant_data <- semi_join(relevant_data, no_dupes, by = c("PersonalID", "DateAdded"))
+  x <- left_join(small_enrollment, relevant_data, by = "PersonalID") 
+  x <- setDT(x)[,.SD[which.max(DateEffective[EntryDate >= DateEffective])], 
+                keyby = EnrollmentID]
+  Enrollment <- left_join(Enrollment, x, 
+                          by = c("PersonalID", "EnrollmentID", "EntryDate", "ExitDate")) %>%
+    select(-DataElement, -DateEffective, -DateAdded) 
 
-x <- setDT(x)[,.SD[which.max(DateEffective[EntryDate >= DateEffective])], keyby = EnrollmentID]
-# Residence Prior
+  return(Enrollment)
+}
 
-# Length Of Stay
-
-# LoS Under Threshhold
-
-# Previous Previous Street ESSH
-
-# Date to Street ESSH
-
-# Times Homeless
-
-# Months Homeless
-
-
+Enrollment <- enrollment_level_value("hud_relationtohoh") %>% rename(RelationshipToHoH = Value)
+Enrollment <- enrollment_level_value("typeoflivingsituation") %>% rename(LivingSituation = Value)
+Enrollment <- enrollment_level_value("hud_lengthofstay") %>% rename(LengthOfStay = Value)
+Enrollment <- enrollment_level_value("hud_lengthstay_less90days") %>% rename(LoS90d = Value)
+Enrollment <- enrollment_level_value("hud_lengthstay_less7nights") %>% rename(LoS7d = Value)
+Enrollment <- enrollment_level_value("hud_nightbeforestreetessh") %>% rename(PreviousStreetESSH = Value)
+Enrollment <- enrollment_level_value("hud_homelessstartdate") %>% rename(DateToStreetESSH = Value)
+Enrollment <- enrollment_level_value("hud_numberoftimestreetessh") %>% rename(TimesHomelessPastThreeYears = Value)
+Enrollment <- enrollment_level_value("hud_nomonthstreetesshin3yrs") %>% rename(MonthsHomelessPastThreeYears = Value)
 
 stage0begin <- now()
 CoC_served <- assessment_data %>%
@@ -216,12 +184,6 @@ endend <- now()
 (alltogether <- endend - stage4begin)
 (thewholething <- endend - stage0begin)
 
-
-residence_prior <- assessment_data %>% filter(DataElement == "typeoflivingsituation")
-length_of_time <- assessment_data %>% filter(DataElement == "hud_lengthofstay")
-LH_prior_90days <- assessment_data %>% filter(DataElement == "hud_lengthstay_less90days")
-LH_prior_7days <- assessment_data %>% filter(DataElement == "hud_lengthstay_less7nights")
-Approx_date_homeless <- assessment_data %>% filter(DataElement == "hud_homelessstartdate")
 DV_yesno <- assessmenet_data %>% filter(DataElement == "domesticviolencevictim")
 DV_when <- assessment_data %>% filter(DataElement == "hud_extentofdv")
 DV_fleeing <- assessment_data %>% filter(DataElement == "hud_extentofdv2")
