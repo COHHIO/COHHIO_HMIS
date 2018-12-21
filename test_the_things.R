ac79933df7d17166bbcbf0376ac8c46a1704a76e
kiadso <genelle@eyedarts.net>
2018-12-21 06:58:42 -0500

    added comment

--- test_the_things.R
+++ test_the_things.R
@@ -3,4 +3,8 @@ get_dupes(Client, PersonalID)
 
 get_dupes(Enrollment, EnrollmentID)
 
+# i don't understand why this doesn't work.
 get_dupes(DomesticViolence, EnrollmentID, DataCollectionStage)
+
+# check that the subassessment dates are all only ymd and make necessary corrections
+
