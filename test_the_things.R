7a9e2033f965fd5c9aee288f2843dc4c72212f8c
kiadso <genelle@eyedarts.net>
2018-12-21 15:48:05 -0500

    got the Enrollment dupes testing to work. added testing for the DV table and some comments.

--- test_the_things.R
+++ test_the_things.R
@@ -1,10 +1,25 @@
 library(janitor)
+
+# Client table testing ----------------------------------------------------
 get_dupes(Client, PersonalID)
 
+View(Client %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
+# Client table is all good
+
+
+# Enrollment table testing ------------------------------------------------
 get_dupes(Enrollment, EnrollmentID)
 
-# i don't understand why this doesn't work.
-get_dupes(DomesticViolence, EnrollmentID, DataCollectionStage)
+View(Enrollment %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
+
+# Domestic Violence testing -----------------------------------------------
+get_dupes(DomesticViolence, DataCollectionStage, EnrollmentID)
+
+View(DomesticViolence %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
+
+
+# Enrollment CoC testing --------------------------------------------------
+
 
 # check that the subassessment dates are all only ymd and make necessary corrections
-
+# can't get the hopwa psh funding source to flip to its number.
