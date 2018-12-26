8dcc666c670a0b80d92bbd84aaa0f78b3d208d32
kiadso <genelle@eyedarts.net>
2018-12-26 16:54:12 -0500

    marked some headers as "ok" and added some comments about things to think about

--- test_the_things.R
+++ test_the_things.R
@@ -3,67 +3,60 @@ library(janitor)
 # can't get the hopwa psh funding source to flip to its number.
 # maybe look at calculating Data Collection Stage by using Intervals like you're
   # doing for NCBs.
+# consider filtering Connection with SOAR data to only pull in PATH and RHY 
+  # Enrollment IDs
 
-# Client table testing ----------------------------------------------------
+# Client table testing -------------------ok-------------------------------
 get_dupes(Client, PersonalID)
 
 View(Client %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
 
 
-# Enrollment table testing ------------------------------------------------
+# Enrollment table testing ---------------ok-------------------------------
 get_dupes(Enrollment, EnrollmentID)
 
 View(Enrollment %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
 
-# Domestic Violence testing -----------------------------------------------
+# Domestic Violence testing --------------ok-------------------------------
 get_dupes(DomesticViolence, DataCollectionStage, EnrollmentID)
 
 View(DomesticViolence %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
 
-
-# Enrollment CoC testing --------------------------------------------------
+# Enrollment CoC testing -----------------ok-------------------------------
 get_dupes(EnrollmentCoC, EnrollmentID, DataCollectionStage)
-View(tablename %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
+View(EnrollmentCoC %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
 
-# EmploymentEducation testing ---------------------------------------------
+# EmploymentEducation testing ------------ok-------------------------------
 get_dupes(EmploymentEducation, EnrollmentID, DataCollectionStage)
-View(tablename %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
+View(EmploymentEducation %>% filter(PersonalID %in% c(81184, 187291, 56556, 177390, 191280)))
 
-
-# Connection w SOAR testing -----------------------------------------------
-get_dupes(tablename, EnrollmentID, DataCollectionStage)
-View(tablename %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
-
+# Connection w SOAR testing --------------ok-------------------------------
+get_dupes(ConnectionWithSOAR, EnrollmentID, DataCollectionStage)
+View(ConnectionWithSOAR %>% filter(PersonalID %in% c(219218, 130592, 196671, 20228, 193399)))
 
 # NonCash testing ---------------------------------------------------------
-get_dupes(tablename, EnrollmentID, DataCollectionStage)
-View(tablename %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
-
+get_dupes(NonCashBenefits, EnrollmentID, DataCollectionStage, BenefitsFromAnySource, NoncashSource)
+View(NonCashBenefits %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
 
 # Disabilities testing ----------------------------------------------------
 get_dupes(tablename, EnrollmentID, DataCollectionStage)
 View(tablename %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
 
-
 # Health Insurance testing ------------------------------------------------
 get_dupes(tablename, EnrollmentID, DataCollectionStage)
 View(tablename %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
-
 
 # Income testing ----------------------------------------------------------
 get_dupes(tablename, EnrollmentID, DataCollectionStage)
 View(tablename %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
 
-
 # MoveInDate testing ------------------------------------------------------
 get_dupes(tablename, EnrollmentID, DataCollectionStage)
 View(tablename %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
 
-
 # Contacts ----------------------------------------------------------------
 get_dupes(tablename, EnrollmentID, DataCollectionStage)
 View(tablename %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
-
 
 # Referrals Services ------------------------------------------------------
 get_dupes(tablename, EnrollmentID, DataCollectionStage)
