a2bc23c259cc9460e980c33b35d72c0237b27869
kiadso <genelle@eyedarts.net>
2019-03-13 16:53:13 -0400

    ditched the rmarkdown bed utilization since it's looking more like i'll be going the Shiny route continued work on the bed utilization script which currently has nothing worked in about dates or months or anything useful like that

---
+++ Bed Unit Utilization.R
@@ -0,0 +1,106 @@
+library(tidyverse)
+library(lubridate)
+load("data/COHHIOHMIS.RData")
+
+# Creating Beds table -----------------------------------------------------
+
+SmallProject <- Project %>%
+  filter(ProjectType %in% c(1, 2, 3, 8, 9) &
+           (OperatingEndDate > today() |
+              is.na(OperatingEndDate)) &
+           is.na(GrantType)) %>%
+  select(ProjectID,
+         ProjectName,
+         OperatingStartDate,
+         OperatingEndDate,
+         ProjectType)
+SmallInventory <- Inventory %>%
+  filter(!is.na(HMISParticipatingBeds) & 
+           CoCCode == "OH-507" &
+           (InventoryEndDate > today() |
+              is.na(InventoryEndDate))) %>%
+  select(
+    ProjectID,
+    HouseholdType,
+    UnitInventory,
+    BedInventory,
+    InventoryStartDate,
+    InventoryEndDate,
+    HMISParticipatingBeds
+  ) 
+Beds <- inner_join(SmallProject, SmallInventory, by = "ProjectID")
+
+# Creating Utilizers table ------------------------------------------------
+
+SmallEnrollment <- Enrollment %>% 
+  select(PersonalID,
+         EnrollmentID,
+         ProjectID,
+         EntryDate,
+         HouseholdID,
+         MoveInDate)
+SmallExit <- Exit %>%
+  select(EnrollmentID, ExitDate)
+Utilizers <- semi_join(SmallEnrollment, Beds, by = "ProjectID") %>%
+  left_join(., SmallExit, by = "EnrollmentID")
+# adding in the Project Type column here to calculate differently based on 
+# MoveInDate
+Utilizers <- left_join(Utilizers, SmallProject, by = "ProjectID") %>%
+  select(
+    PersonalID,
+    EnrollmentID,
+    ProjectID,
+    ProjectType,
+    HouseholdID,
+    EntryDate,
+    MoveInDate,
+    ExitDate
+  )
+
+# Cleaning up the house ---------------------------------------------------
+
+save(Beds, Utilizers, file = "data/BedUtilization.Rdata")
+rm(list = ls())
+load("data/BedUtilization.Rdata")
+
+# Bed Nights Utilized -----------------------------------------------------
+
+# filtering out any PSH or RRH records without a proper Move-In Date plus the 
+# fake training providers
+Utilizers <- Utilizers %>%
+  mutate(ExitAdjust = if_else(is.na(ExitDate), today(), ExitDate)) %>%
+  filter((
+    (
+      ProjectType %in% c(3, 9, 13) &
+        !is.na(MoveInDate) &
+        ymd(MoveInDate) < ymd(EntryDate) &
+        ymd(MoveInDate) > ymd(ExitAdjust)
+    ) |
+      ProjectType %in% c(1, 2, 8)
+  ) &
+    !ProjectID %in% c(1775, 1695, 1849, 1032, 1030, 1031, 1317))
+# adding in calculated columns to help get to Bed Nights
+Utilizers <- Utilizers %>%
+  mutate(
+    BedNights = case_when(
+      ProjectType %in% c(1, 2, 8) ~
+        difftime(ymd(ExitAdjust), ymd(EntryDate),
+                 units = "days"),
+      ProjectType %in% c(3, 9, 13) ~
+        difftime(ymd(ExitAdjust), ymd(MoveInDate),
+                 units = "days")
+    )
+  ) %>%
+  filter(BedNights > 0)
+# bare bones list of bed nights per provider (no date ranges)
+Utilization <- Utilizers %>% select(ProjectID, BedNights) %>% 
+  group_by(ProjectID) %>% 
+  summarise(BedNights = sum(BedNights))
+
+# Possible Bed Nights -----------------------------------------------------
+
+BedNights <- Beds %>%
+  select(ProjectID, BedInventory) %>%
+  group_by(ProjectID) %>%
+  summarise(BedInventory =
+              sum(BedInventory) * 30)
