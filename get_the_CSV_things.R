74722db5cd10e33561d0cf627c104db330c38f97
kiadso <genelle@eyedarts.net>
2019-03-25 08:52:20 -0400

    file used to get DA the children and baby counts for 2017-2018

--- get_the_CSV_things.R
+++ get_the_CSV_things.R
@@ -107,5 +107,23 @@ Client <- Client %>%
     !is.na(SSN) ~ SSN
   ))
 
+# Age Function ------------------------------------------------------------
+
+age_years <- function(earlier, later)
+{
+  lt <- data.frame(earlier, later)
+  age <- as.numeric(format(lt[,2],format="%Y")) - as.numeric(format(lt[,1],format="%Y"))
+  
+  dayOnLaterYear <- ifelse(format(lt[,1],format="%m-%d")!="02-29",
+                           as.Date(paste(format(lt[,2],format="%Y"),"-",format(lt[,1],format="%m-%d"),sep="")),
+                           ifelse(as.numeric(format(later,format="%Y")) %% 400 == 0 | as.numeric(format(later,format="%Y")) %% 100 != 0 & as.numeric(format(later,format="%Y")) %% 4 == 0,
+                                  as.Date(paste(format(lt[,2],format="%Y"),"-",format(lt[,1],format="%m-%d"),sep="")),
+                                  as.Date(paste(format(lt[,2],format="%Y"),"-","02-28",sep=""))))
+  
+  age[which(dayOnLaterYear > lt$later)] <- age[which(dayOnLaterYear > lt$later)] - 1
+  
+  age
+}
+
 # Save it out -------------------------------------------------------------
 save.image(file = "data/COHHIOHMIS.RData")
