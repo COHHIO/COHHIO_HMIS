0207e578d496731b85ad74581a1c1ba21d19f46b
kiadso <genelle@eyedarts.net>
2019-03-25 20:20:54 -0400

    added more functions but they are rough drafts that i can't test at home so test them at work

--- get_the_CSV_things.R
+++ get_the_CSV_things.R
@@ -114,16 +114,59 @@ age_years <- function(earlier, later)
   lt <- data.frame(earlier, later)
   age <- as.numeric(format(lt[,2],format="%Y")) - as.numeric(format(lt[,1],format="%Y"))
   
-  dayOnLaterYear <- ifelse(format(lt[,1],format="%m-%d")!="02-29",
-                           as.Date(paste(format(lt[,2],format="%Y"),"-",format(lt[,1],format="%m-%d"),sep="")),
-                           ifelse(as.numeric(format(later,format="%Y")) %% 400 == 0 | as.numeric(format(later,format="%Y")) %% 100 != 0 & as.numeric(format(later,format="%Y")) %% 4 == 0,
-                                  as.Date(paste(format(lt[,2],format="%Y"),"-",format(lt[,1],format="%m-%d"),sep="")),
-                                  as.Date(paste(format(lt[,2],format="%Y"),"-","02-28",sep=""))))
+  dayOnLaterYear <- ifelse(
+    format(lt[, 1], format = "%m-%d") != "02-29",
+    as.Date(paste(
+      format(lt[, 2], format = "%Y"), "-",
+      format(lt[, 1], format = "%m-%d"),
+      sep = ""
+    )),
+    ifelse(
+      as.numeric(format(later, format = "%Y")) %%
+        400 == 0 |
+        as.numeric(format(later,
+                          format =
+                            "%Y")) %%
+        100 != 0 &
+        as.numeric(format(later, format = "%Y")) %%
+        4 == 0,
+      as.Date(paste(
+        format(lt[, 2], format = "%Y"),
+        "-",
+        format(lt[, 1], format =
+                 "%m-%d"),
+        sep = ""
+      )),
+      as.Date(paste(
+        format(lt[, 2], format = "%Y"),
+        "-",
+        "02-28",
+        sep = ""
+      ))
+    )
+  )
   
   age[which(dayOnLaterYear > lt$later)] <- age[which(dayOnLaterYear > lt$later)] - 1
   
   age
 }
+
+# Served Between Date Range Function --------------------------------------
+
+# servedbetween <- function(start, end){
+#   served <- ymd(EntryDate) <= ymd(end) &
+#     (is.na(ExitDate) | ymd(ExitDate) >= ymd(start))
+#   served
+# }
+
+
+# Operating Between Date Range Function -----------------------------------
+
+# operatingbetween <- function(start, end) {
+#   operating <- ymd(OperatingStartDate) <= ymd(end) &
+#     (is.na(OperatingEndDate) >= ymd(start))
+#   operating
+# }
 
 # Save it out -------------------------------------------------------------
 save.image(file = "data/COHHIOHMIS.RData")
