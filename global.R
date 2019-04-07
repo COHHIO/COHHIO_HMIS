171aab9d5fad38df40021b93123c2ad02bea92c4
kiadso <genelle@eyedarts.net>
2019-04-06 22:11:06 -0400

    added shinyWidgets because i really like the picklist much better than the stock one lifted some code from the Bed Unit Utilization script and modded it to only care about "today" so i could add it to the provider dashboard

--- global.R
+++ global.R
@@ -1,5 +1,63 @@
 library(tidyverse)
 library(shiny)
+library(shinyWidgets)
 library(lubridate)
 
 load("data/COHHIOHMIS.Rdata")
+
+providerids <- Project %>% 
+  select(ProjectID, ProjectName, OrganizationName) %>%
+  arrange(ProjectName)
+
+# load("data/Utilization.Rdata")
+
+SmallProject <- Project %>%
+  filter(ProjectType %in% c(1, 2, 3, 8, 9) &
+           ymd(OperatingStartDate) <= today() &
+           (is.na(OperatingEndDate) | OperatingEndDate >= today()) &
+           is.na(Project$GrantType)) %>%
+  select(ProjectID,
+         ProjectName,
+         ProjectType)
+
+SmallInventory <- Inventory %>%
+  filter((ymd(InventoryStartDate) <= today() &
+            (
+              ymd(InventoryEndDate) >= today() |
+                is.na(InventoryEndDate)
+            )) &
+           !is.na(HMISParticipatingBeds) &
+           Inventory$CoCCode == "OH-507") %>%
+  select(
+    ProjectID,
+    HouseholdType,
+    UnitInventory,
+    BedInventory,
+    InventoryStartDate,
+    InventoryEndDate,
+    HMISParticipatingBeds
+  )
+
+SmallInventory <- inner_join(SmallProject, SmallInventory, by = "ProjectID")
+
+UnitCapacity <- SmallInventory %>%
+  select(ProjectID,
+         ProjectName,
+         ProjectType,
+         HouseholdType,
+         UnitInventory,
+         BedInventory,
+         InventoryStartDate,
+         InventoryEndDate) %>%
+  mutate(UnitCount = if_else(HouseholdType == 3,
+                             UnitInventory, BedInventory)) %>%
+  group_by(ProjectID, ProjectName, ProjectType) %>%
+  summarise(UnitCount = sum(UnitCount)) %>%
+  ungroup()
+
+
+
+
+
+
+
