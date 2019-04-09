852527eb44ceff59b38a4d44d5407679cf1f383f
Genelle Denzin <genelle@eyedarts.net>
2019-04-09 14:05:22 -0400

    added crude chronicity calculation

--- Prioritization.R
+++ Prioritization.R
@@ -5,3 +5,16 @@ load("data/COHHIOHMIS.Rdata")
 
 rm(Affiliation, EmploymentEducation, EnrollmentCoC, Export, ProjectCoC, Services)
 
+# run on a geography, not project(s)
+
+Chronicity <- 
+  Enrollment %>%
+  filter(
+    served_between(Enrollment, ReportStart, ReportEnd) &
+      ((ymd(DateToStreetESSH) + years(1) <= ymd(EntryDate)) |
+      (
+        MonthsHomelessPastThreeYears %in% c(112, 113) &
+          TimesHomelessPastThreeYears == 4
+      )) &
+      DisablingCondition == 1
+  )
