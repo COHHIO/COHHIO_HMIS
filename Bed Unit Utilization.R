0e3cdd25f979fd54e5d9b0d2275a5b2d5becad8f
Genelle Denzin <genelle@eyedarts.net>
2019-04-09 11:20:55 -0400

    added current utilization logic from global to here so utilization can all just come from the same script

--- Bed Unit Utilization.R
+++ Bed Unit Utilization.R
@@ -66,10 +66,10 @@ Utilizers <- left_join(Utilizers, SmallProject, by = "ProjectID") %>%
   )
 # Cleaning up the house ---------------------------------------------------
 
-rm(Affiliation, Client, Disabilities, EmploymentEducation, Enrollment,
-   EnrollmentCoC, Exit, Export, Funder, Geography, HealthAndDV, IncomeBenefits,
-   Inventory, Organization, Project, ProjectCoC, Scores, Services, 
-   SmallEnrollment, SmallInventory, SmallProject, Users, Offers, VeteranCE)
+rm(Affiliation, Client, Disabilities, EmploymentEducation, EnrollmentCoC, Exit, 
+   Export, Funder, Geography, HealthAndDV, IncomeBenefits, Organization, 
+   ProjectCoC, Scores, Services, SmallEnrollment, SmallInventory, SmallProject, 
+   Users, Offers, VeteranCE)
 # Client Utilization of Beds ----------------------------------------------
 
 # filtering out any PSH or RRH records without a proper Move-In Date plus the 
@@ -459,5 +459,81 @@ names(UnitUtilization) <-
     month.name[(month(ymd(int_start(TwelfthMonth))))])
 
 rm(bed_capacity, bed_nights_per_ee, unit_capacity)
+
+SmallProject <- Project %>%
+  filter(ProjectType %in% c(1, 2, 3, 8, 9, 13) &
+           ymd(OperatingStartDate) <= today() &
+           (is.na(OperatingEndDate) | OperatingEndDate >= today()) &
+           is.na(Project$GrantType)) %>%
+  select(ProjectID,
+         ProjectName,
+         ProjectType, 
+         OrganizationName)
+
+SmallInventory <- Inventory %>%
+  filter((ymd(InventoryStartDate) <= today() &
+            (
+              ymd(InventoryEndDate) >= today() |
+                is.na(InventoryEndDate)
+            )) &
+           !is.na(HMISParticipatingBeds) &
+           Inventory$CoCCode == "OH-507") %>%
+  select(
+    ProjectID,
+    HouseholdType,
+    UnitInventory,
+    BedInventory,
+    InventoryStartDate,
+    InventoryEndDate,
+    HMISParticipatingBeds
+  )
+
+SmallInventory <- inner_join(SmallProject, SmallInventory, by = "ProjectID")
+
+Capacity <- SmallInventory %>%
+  select(ProjectID,
+         ProjectName,
+         ProjectType,
+         OrganizationName,
+         HouseholdType,
+         UnitInventory,
+         BedInventory,
+         InventoryStartDate,
+         InventoryEndDate) %>%
+  mutate(UnitCount = if_else(HouseholdType == 3,
+                             UnitInventory, BedInventory)) %>%
+  group_by(ProjectID, ProjectName, ProjectType, OrganizationName) %>%
+  summarise(UnitCount = sum(UnitCount),
+            BedCount = sum(BedInventory)) %>%
+  ungroup()
+
+providerids <- Capacity %>% 
+  select(ProjectID, ProjectName, OrganizationName) %>%
+  arrange(ProjectName)
+
+Clients <- Enrollment %>%
+  left_join(., providerids, by = "ProjectID") %>%
+  filter(is.na(ExitDate)) %>%
+  group_by(ProjectID, ProjectName) %>%
+  summarise(Clients = n_distinct(PersonalID)) %>%
+  ungroup()
+
+Households <- Enrollment %>%
+  left_join(., providerids, by = "ProjectID") %>%
+  filter(is.na(ExitDate)) %>%
+  group_by(ProjectID, ProjectName) %>%
+  summarise(Households = n_distinct(HouseholdID)) %>%
+  ungroup()
+
+Utilization <-
+  left_join(Capacity, Clients,
+            by = c("ProjectID", "ProjectName")) %>%
+  left_join(., Households, 
+            by = c("ProjectID", "ProjectName")) %>%
+  filter(ProjectType %in% c(1, 2, 3, 8, 9)) %>%
+  mutate(BedUtilization = percent(Clients/BedCount),
+         UnitUtilization = percent(Households/UnitCount))
+rm(Households, Clients, Capacity, Enrollment, Project, Inventory, 
+   SmallInventory, SmallProject)
 
 save.image("data/Utilization.RData")
