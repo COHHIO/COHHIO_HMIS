1983dfb8641613a7503e9c224f2c52b267a9dbb1
kiadso <genelle@eyedarts.net>
2019-04-17 07:47:58 -0400

    changed the look and order of some of the the objects tried to figure out how to use the inputs in logic but no dice (yet)

--- experiment_dates_shiny.R
+++ experiment_dates_shiny.R
@@ -4,24 +4,27 @@ library(lubridate)
 
 if (interactive()) {
   ui <- fluidPage(
-    sliderInput("n", "Quarter", 1, 4, 
-                if_else(quarter(today()) - 1 == 0, 1,
-                        quarter(today()) - 1)),
-    sliderInput("y", "Year", year(today()) - 2, 
+    dateRangeInput("inDateRange", "Input date range"),
+    sliderInput("y", "", year(today()) - 2, 
                 year(today()), 
                 year(today()),
-                sep = ""),
-    dateRangeInput("inDateRange", "Input date range")
+                sep = "",
+                ticks = FALSE),    
+    sliderInput("q", "", 1, 4, 
+                if_else(quarter(today()) - 1 == 0, 1,
+                        quarter(today()) - 1),
+                ticks = FALSE,
+                pre = "Q")
   )
   
   server <- function(input, output, session) {
     observe({
       enddate <- mdy(paste(
         case_when(
-          input$n == 1 ~ "03/31/",
-          input$n == 2 ~ "06/30/",
-          input$n == 3 ~ "09/30/",
-          input$n == 4 ~ "12/31/"
+          input$q == 1 ~ "03/31/",
+          input$q == 2 ~ "06/30/",
+          input$q == 3 ~ "09/30/",
+          input$q == 4 ~ "12/31/"
         ),
         input$y
       ))
@@ -29,7 +32,7 @@ if (interactive()) {
       updateDateRangeInput(
         session,
         "inDateRange",
-        label = paste("Year", input$y,"Q", input$n),
+        label = paste("Year", input$y,"Q", input$q),
         start = mdy(paste("01/01/", input$y)),
         end = enddate
       )
