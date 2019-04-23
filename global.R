83c74853d838c50ce1a2fae6f7431f6718c15a52
Genelle Denzin <genelle@eyedarts.net>
2019-04-23 17:00:29 -0400

    moved code that requires dates to the Global file in the app. not its final home though, still need to get it all into the server file, just not sure how yet.

--- global.R
+++ global.R
@@ -10,3 +10,33 @@ updatedate <- file.info("data/COHHIOHMIS.RData")$mtime
 load("data/Utilization.RData")
 
 load("data/QPR_SPDATs.RData")
+
+ClientScoresInCounty <- CountyData %>%
+  filter(served_between(CountyData, ReportStart, ReportEnd)) %>%
+  select(CountyServed, PersonalID, Score) %>%
+  distinct()
+
+CountyAverageScores <- ClientScoresInCounty %>%
+  group_by(CountyServed) %>%
+  summarise(AverageScore = round(mean(Score), 1), 
+            HHsLHinCounty = n())
+
+ProviderAverages <- SPDATsByProject %>%
+  filter(served_between(SPDATsByProject, ReportStart, ReportEnd)) %>%
+  select(EnrollmentID, ProjectName, ScoreAdjusted) %>%
+  group_by(ProjectName) %>%
+  summarise(AverageScore = round(mean(ScoreAdjusted), 1),
+            EnrollmentCount = n())
+
+CountyHousedAverageScores <- SPDATsByProject %>%
+  filter(served_between(SPDATsByProject, ReportStart, ReportEnd)) %>%
+  group_by(CountyServed) %>%
+  summarise(HousedAverageScore = round(mean(ScoreAdjusted), 1),
+            HHsHousedInCounty = n())
+
+Compare <- 
+  full_join(CountyAverageScores, 
+            CountyHousedAverageScores, 
+            by = "CountyServed") %>%
+  arrange(CountyServed) %>%
+  left_join(., Regions, by = c("CountyServed" = "County"))
