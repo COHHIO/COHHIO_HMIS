773245c3cf9fb662033a2103704cc2c8b7de1d63
Genelle Denzin <genelle@eyedarts.net>
2019-04-30 17:23:36 -0400

    removed a bunch of stuff that had been commented out since everything works now

--- global.R
+++ global.R
@@ -11,30 +11,10 @@ load("data/Utilization.RData")
 
 load("data/QPR_SPDATs.RData")
 
-# CountyAverageScores <- CountyData %>%
-#   filter(served_between(CountyData, ReportStart, ReportEnd)) %>%
-#   select(CountyServed, PersonalID, Score) %>%
-#   distinct() %>%
-#   group_by(CountyServed) %>%
-#   summarise(AverageScore = round(mean(Score), 1), 
-#             HHsLHinCounty = n())
-# 
-# # ProviderAverages <- SPDATsByProject %>%
-# #   filter(served_between(SPDATsByProject, ReportStart, ReportEnd)) %>%
-# #   select(EnrollmentID, ProjectName, ScoreAdjusted) %>%
-# #   group_by(ProjectName) %>%
-# #   summarise(AverageScore = round(mean(ScoreAdjusted), 1),
-# #             EnrollmentCount = n())
-# 
-# CountyHousedAverageScores <- SPDATsByProject %>%
-#   filter(served_between(SPDATsByProject, ReportStart, ReportEnd)) %>%
-#   group_by(CountyServed) %>%
-#   summarise(HousedAverageScore = round(mean(ScoreAdjusted), 1),
-#             HHsHousedInCounty = n())
-# 
-# Compare <- 
-#   full_join(CountyAverageScores, 
-#             CountyHousedAverageScores, 
-#             by = "CountyServed") %>%
-#   arrange(CountyServed) %>%
-#   left_join(., Regions, by = c("CountyServed" = "County"))
+ProviderAverages <- SPDATsByProject %>%
+  filter(served_between(SPDATsByProject, ReportStart, ReportEnd)) %>%
+  select(EnrollmentID, ProjectName, ScoreAdjusted) %>%
+  group_by(ProjectName) %>%
+  summarise(AverageScore = round(mean(ScoreAdjusted), 1),
+            EnrollmentCount = n())
+
