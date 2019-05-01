4ddf02ae31d815c57241e5db0bf7a741640540b5
kiadso <genelle@eyedarts.net>
2019-05-01 17:08:51 -0400

    added comments and household calculations

--- LSA 27180.R
+++ LSA 27180.R
@@ -1,6 +1,10 @@
-projectx <- ClientUtilizers %>% filter(ProjectID == 878)
+# run the Bed Unit Utilization script down to the point where it creates the 
+# ClientUtilizers object
 
-projectx <- projectx %>% mutate(daysinproject = difftime(ExitAdjust, EntryAdjust, units = "days"))
+projectx <- ClientUtilizers %>% filter(ProjectID == 1017)
+
+projectx <- projectx %>% 
+  mutate(daysinproject = difftime(ExitAdjust, EntryAdjust, units = "days"))
 
 projectx %>% group_by(ProjectID) %>% summarise(avg = mean(daysinproject))
 
@@ -8,3 +12,8 @@ projectxSingles <- projectx %>% filter(grepl("s_", HouseholdID))
 
 projectxSingles %>% group_by(ProjectID) %>% summarise(avg = mean(daysinproject))
 
+projectxHHs <- projectx %>% filter(grepl("h_", HouseholdID))
+
+projectxHHs %>% group_by(ProjectID) %>% summarise(avg = mean(daysinproject))
+
+rm(projectx, projectxSingles, projectxHHs)
