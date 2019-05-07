4c7001b70152e74ff130db86788326633a49ed8d
kiadso <genelle@eyedarts.net>
2019-05-07 16:50:36 -0400

    changed output so it's more readable

--- RecurrenceProjectEvaluation.R
+++ RecurrenceProjectEvaluation.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(readxl)
+
 # getting the Recurrence data for Project Evaluation
 # run the ART report called Recurrence for Export in the Project Evaluation>2017
 # remove the spaces and apostrophes out of the headers
@@ -22,7 +25,11 @@ recurrence <-
     )
   ) %>%
   group_by(Provider, ProjectType, DaysBetweenStays) %>%
-  summarise(recurred = n())
+  summarise(recurred = n()) %>%
+  ungroup()
+
+recurrence <- recurrence %>% spread(DaysBetweenStays, recurred) %>%
+  rename("UpToSixMonths" = `1`, "UpTo2yrs" = `2`)
 
 served <- served %>%
   filter(!`Project Type Code` == "Emergency Shelter (HUD)") %>%
@@ -30,96 +37,80 @@ served <- served %>%
 
 recurrence <- left_join(recurrence, served, by = "Provider") %>%
   mutate(
-    percent = recurred / PermanentExits,
-    points = case_when(
-# TH & RRH
+    percent0to6 = UpToSixMonths / PermanentExits,
+    percent2yrs = UpTo2yrs / PermanentExits,
+    points0to6 = case_when(
+      # TH & RRH
       ProjectType %in% c(13, 2) &
-        DaysBetweenStays == 1 &
-        percent <= .07 ~ 10,
+        percent0to6 <= .07 ~ 10,
       ProjectType %in% c(13, 2) &
-        DaysBetweenStays == 1 &
-        percent <= .09 &
-        percent > .07 ~ 7.5,
+        percent0to6 <= .09 &
+        percent0to6 > .07 ~ 7.5,
       ProjectType %in% c(13, 2) &
-        DaysBetweenStays == 1 &
-        percent <= .12 &
-        percent > .09 ~ 5,
+        percent0to6 <= .12 &
+        percent0to6 > .09 ~ 5,
       ProjectType %in% c(13, 2) &
-        DaysBetweenStays == 1 &
-        percent > .12 ~ 0,
+        percent0to6 > .12 ~ 0,
+      # PSH
+      ProjectType %in% c(3, 9) &
+        percent0to6 <= .02 ~ 10,
+      ProjectType %in% c(3, 9) &
+        percent0to6 <= .04 &
+        percent0to6 > .02 ~ 7.5,
+      ProjectType %in% c(3, 9) &
+        percent0to6 <= .06 &
+        percent0to6 > .04 ~ 5,
+      ProjectType %in% c(3, 9) &
+        percent0to6 > .06 ~ 0,
+      # SH
+      ProjectType == 8 &
+        percent0to6 <= .15 ~ 10,
+      ProjectType == 8 &
+        percent0to6 <= .17 &
+        percent0to6 > .15 ~ 7.5,
+      ProjectType == 8 &
+        percent0to6 <= .19 &
+        percent0to6 > .17 ~ 5,
+      ProjectType == 8 &
+        percent0to6 > .19 ~ 0
+    ),
+    points2yrs = case_when(
+      # TH & RRH
       ProjectType %in% c(13, 2) &
-        DaysBetweenStays == 2 &
-        percent <= .12 ~ 10,
+        percent2yrs <= .12 ~ 10,
       ProjectType %in% c(13, 2) &
-        DaysBetweenStays == 2 &
-        percent <= .14 &
-        percent > .12 ~ 7.5,
+        percent2yrs <= .14 &
+        percent2yrs > .12 ~ 7.5,
       ProjectType %in% c(13, 2) &
-        DaysBetweenStays == 2 &
-        percent <= .17 &
-        percent > .14 ~ 5,
+        percent2yrs <= .17 &
+        percent2yrs > .14 ~ 5,
       ProjectType %in% c(13, 2) &
-        DaysBetweenStays == 2 &
-        percent > .17 ~ 0,
-# PSH
+        percent2yrs > .17 ~ 0,
+      # PSH
       ProjectType %in% c(3, 9) &
-        DaysBetweenStays == 1 &
-        percent <= .02 ~ 10,
+        percent2yrs <= .05 ~ 10,
       ProjectType %in% c(3, 9) &
-        DaysBetweenStays == 1 &
-        percent <= .04 &
-        percent > .02 ~ 7.5,
+        percent2yrs <= .08 &
+        percent2yrs > .05 ~ 7.5,
       ProjectType %in% c(3, 9) &
-        DaysBetweenStays == 1 &
-        percent <= .06 &
-        percent > .04 ~ 5,
+        percent2yrs <= .09 &
+        percent2yrs > .08 ~ 5,
       ProjectType %in% c(3, 9) &
-        DaysBetweenStays == 1 &
-        percent > .06 ~ 0,
-      ProjectType %in% c(3, 9) &
-        DaysBetweenStays == 2 &
-        percent <= .05 ~ 10,
-      ProjectType %in% c(3, 9) &
-        DaysBetweenStays == 2 &
-        percent <= .08 &
-        percent > .05 ~ 7.5,
-      ProjectType %in% c(3, 9) &
-        DaysBetweenStays == 2 &
-        percent <= .09 &
-        percent > .08 ~ 5,
-      ProjectType %in% c(3, 9) &
-        DaysBetweenStays == 2 &
-        percent > .09 ~ 0,
-# SH
+        percent2yrs > .09 ~ 0,
+      # SH
       ProjectType == 8 &
-        DaysBetweenStays == 2 &
-        percent <= .2 ~ 10,
+        percent2yrs <= .2 ~ 10,
       ProjectType == 8 &
-        DaysBetweenStays == 2 &
-        percent <= .22 &
-        percent > .2 ~ 7.5,
+        percent2yrs <= .22 &
+        percent2yrs > .2 ~ 7.5,
       ProjectType == 8 &
-        DaysBetweenStays == 2 &
-        percent <= .24 &
-        percent > .22 ~ 5,
+        percent2yrs <= .24 &
+        percent2yrs > .22 ~ 5,
       ProjectType == 8 &
-        DaysBetweenStays == 2 &
-        percent > .24 ~ 0,
-      ProjectType == 8 &
-        DaysBetweenStays == 1 &
-        percent <= .15 ~ 10,
-      ProjectType == 8 &
-        DaysBetweenStays == 1 &
-        percent <= .17 &
-        percent > .15 ~ 7.5,
-      ProjectType == 8 &
-        DaysBetweenStays == 1 &
-        percent <= .19 &
-        percent > .17 ~ 5,
-      ProjectType == 8 &
-        DaysBetweenStays == 1 &
-        percent > .19 ~ 0
+        percent2yrs > .24 ~ 0
     )
-  )
+  ) 
+
+
 
 write_csv(recurrence, "data/RecurrenceProjectEvaluation.csv")
