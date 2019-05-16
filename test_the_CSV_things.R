879a298bbf07b39210560c3c301343680ae2bcd5
Genelle Denzin <genelle@eyedarts.net>
2019-05-15 23:23:55 -0400

    worked on some testing logic for the HUD CSV export

--- test_the_CSV_things.R
+++ test_the_CSV_things.R
@@ -3,13 +3,92 @@ library(janitor)
 library(skimr)
 library(tidylog)
 
+load("images/COHHIOHMIS.RData")
+
 # Client table testing ----------------------------------------------------
-get_dupes(Client, PersonalID)
+context('testing client file')
 
-View(Client %>% filter(PersonalID %in% c(105108, 54017, 188869, 192304, 144707)))
-summary(Client)
+test_that('client file is the right shape', {
+  expect_equal(ncol(Client), 33, label = "Wrong number of columns")
+  expect_gte(nrow(Client), 35000, label = "Maybe not incomplete data set")
+  expect_equal(nrow(get_dupes(Client, PersonalID)), 0, label = "Duplicate Client IDs")
+})
 
-skim(Client)
+test_that('data seems complete', {
+  expect_equal(nrow(
+    filter(Client, RaceNone %in% c(8, 9, 99) |
+             AmIndAKNative + Asian + BlackAfAmerican + NativeHIOtherPacific +
+             White != 0)), 
+               nrow(Client),
+    label = "RaceNone not calculated correctly")
+  expect_equal(nrow(filter(Client, is.na(VeteranStatus))), 0, 
+               label = "Null Veteran Status")
+})
+
+fromART <- read_csv("data/BOST_Client_.csv")
+
+smallFromART <- fromART %>% mutate(
+  AmIndAKNative =
+    if_else(
+      Race1 == "American Indian or Alaska Native (HUD)" |
+        Race2 == "American Indian or Alaska Native (HUD)",
+      1,
+      0,
+      missing = 0
+    ),
+  Asian =
+    if_else(Race1 == "Asian (HUD)" |
+              Race2 == "Asian (HUD)", 1, 0, missing = 0),
+  BlackAfAmerican =
+    if_else(
+      Race1 == "Black or African American (HUD)" |
+        Race2 == "Black or African American (HUD)",
+      1,
+      0,
+      missing = 0
+    ),
+  NativeHIOtherPacific =
+    if_else(
+      Race1 == "Native Hawaiian or Other Pacific Islander (HUD)" |
+        Race2 == "Native Hawaiian or Other Pacific Islander (HUD)",
+      1,
+      0,
+      missing = 0
+    ),
+  White =
+    if_else(Race1 == "White (HUD)" |
+              Race2 == "White (HUD)", 1, 0, missing = 0),
+  Ethnicity = case_when(
+    Ethnicity == "Non-Hispanic/Non-Latino (HUD)" ~ 0,
+    Ethnicity == "Hispanic/Latino (HUD)" ~ 1,
+    Ethnicity == "Client doesn't know (HUD)" ~ 8,
+    Ethnicity == "Client refused (HUD)" ~ 9,
+    Ethnicity == "Data not collected (HUD)" | is.na(Ethnicity) ~ 99
+    ),
+  Gender = case_when(
+    Gender == "Female" ~ 0,
+    Gender == "Male" ~ 1,
+    Gender == "Trans Male (FTM or Female to Male)" ~ 3,
+    Gender == "Trans Female (MTF or Male to Female)" ~ 2,
+    Gender == "Gender Non-Conforming (i.e. not exclusively male or female)" ~ 4,
+    Gender == "Client doesn't know" ~ 8,
+    Gender == "Client refused" ~ 9,
+    Gender == "Data not collected" | is.na(Gender) ~ 99
+  ),
+  VeteranStatus = case_when(
+    VeteranStatus == "No (HUD)" ~ 0,
+    VeteranStatus == "Yes (HUD)" ~ 1,
+    VeteranStatus == "Client doesn't know (HUD)" ~ 8,
+    VeteranStatus == "Client refused (HUD)" ~ 9,
+    VeteranStatus == "Data not collected (HUD)" | is.na(VeteranStatus) ~ 99)
+) %>% select(ClientID, ClientSSN, DOB, Ethnicity, Gender, VeteranStatus)
+
+smallClient <- 
+  Client %>% 
+  select(PersonalID, SSN, DOB, Ethnicity, Gender, VeteranStatus)
+
+map2(smallClient, smallFromART, setdiff) %>% 
+  map_int(length)
 
 # Enrollment table testing ------------------------------------------------
 get_dupes(Enrollment, EnrollmentID)
