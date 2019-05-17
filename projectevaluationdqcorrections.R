dee860421b68fdaa84238c9e208bcf15f3e240af
kiadso <genelle@eyedarts.net>
2019-05-17 15:32:49 -0400

    more explaining and better naming

--- projectevaluationdqcorrections.R
+++ projectevaluationdqcorrections.R
@@ -10,11 +10,12 @@ y <- read_xlsx("data/dataqualitypointscorrected.xlsx",
 
 z <- y %>% left_join(x, by = c("Project" = "Provider")) %>%
   mutate(DQScoreOLD = if_else(is.na(DQScoreOLD), 0, DQScoreOLD),
-         Points = if_else(is.na(Points), 0, Points))
-
-a <- z %>%
-  filter(Points != DQScoreOLD)
-
-b <- z %>% filter(DQScoreOLD > Points)
-
-c <- z %>% filter(DQScoreOLD < Points)
+         CorrectedPoints = if_else(is.na(Points), 0, Points),
+         Points = NULL)
+# all projects that don't match old points to new points
+scoresNotTheSame <- z %>%
+  filter(CorrectedPoints != DQScoreOLD)
+# these projects should not lose points as they were adjusted for consolidation
+theyLostPoints <- z %>% filter(DQScoreOLD > CorrectedPoints)
+# these projects should gain the points (sent to Erica)
+theyGainedPoints <- z %>% filter(DQScoreOLD < CorrectedPoints)
