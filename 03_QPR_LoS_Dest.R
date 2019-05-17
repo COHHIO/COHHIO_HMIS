1569380cf44c1ade9f012d11a53e4a3bd78f146c
kiadso <genelle@eyedarts.net>
2019-05-17 15:36:33 -0400

    accounted for the fact that HP and Outreach projects need to be included but they don't have HMIS participating beds so.. they have to be sort of forced into the dataset. added MoveInDate so I could add an EntryAdjust field for PSH and RRH projects filtered at the top to only look at households changed the name of the main object from "Destination" which is a variable name already to QPREEs so it's more general and can be used for Length of Stay as well moved some stuff around so it's more organized

--- 03_QPR_LoS_Dest.R
+++ 03_QPR_LoS_Dest.R
@@ -11,7 +11,22 @@ load("images/COHHIOHMIS.RData")
 smallProject <- Project %>%
   select(ProjectID,
          ProjectName,
-         ProjectType)
+         ProjectType) 
+
+hmisbeds <- Inventory %>%
+  filter(HMIS_participating_between(Inventory, FileStart, FileEnd)) %>%
+  select(ProjectID) %>% unique()
+
+hpOutreach <- Project %>% 
+  filter(ProjectType %in% c(4, 12) &
+           operating_between(., FileStart, FileEnd)) %>% 
+  select(ProjectID)
+
+x <- rbind(hmisbeds, hpOutreach)
+
+smallProject <- smallProject %>% semi_join(x, by = "ProjectID") 
+
+smallProject <- as.data.frame(smallProject)
 
 smallEnrollment <- Enrollment %>%
   select(
@@ -22,45 +37,70 @@ smallEnrollment <- Enrollment %>%
     RelationshipToHoH,
     CountyServed,
     EntryDate,
+    MoveInDate,
     ExitDate,
     ExitAdjust,
     Destination
-  )
+  ) %>%
+  filter(str_detect(HouseholdID, fixed("s_")) |
+           (str_detect(HouseholdID, fixed("h_")) &
+              RelationshipToHoH == 1))
 
-Destination <- smallEnrollment %>%
-  left_join(smallProject, by = "ProjectID") %>%
-  filter(!is.na(ExitDate)) %>%
+smallEnrollment <- as.data.frame(smallEnrollment)
+# captures all leavers PLUS all ee's in either HP or PSH
+# also limits records to singles and HoHs only
+QPREEs <- smallProject %>%
+  left_join(smallEnrollment, by = "ProjectID") %>%
+  filter((!is.na(ExitDate) | ProjectType %in% c(3, 9, 12)) &
+           served_between(., FileStart, FileEnd)) %>%
   mutate(
     DestinationGroup = case_when(
-      Destination %in% c(1, 2, 12, 13, 16, 18, 27, 14) ~ "Temporary",
+      Destination %in% c(1, 2, 12, 13, 14, 16, 18, 27) ~ "Temporary",
       Destination %in% c(3, 10, 11, 19:23, 28, 31) ~ "Permanent",
       Destination %in% c(4:7, 15, 25:27, 29) ~ "Institutional",
       Destination %in% c(8, 9, 17, 24, 30, 99) ~ "Other"
-    )
+    ),
+    EntryAdjust = case_when(
+      ProjectType %in% c(1, 2, 8) ~ EntryDate,
+      ProjectType %in% c(3, 9, 13) ~ MoveInDate),
+    DaysinProject = difftime(ExitAdjust, EntryAdjust, units = "days")
   )
 
+PermAndRetention <- QPREEs %>%
+  filter((DestinationGroup == "Permanent" | is.na(ExitDate)) &
+           ProjectType %in% c(3, 9, 12))
+
+PermLeavers <- QPREEs %>%
+  filter(DestinationGroup == "Permanent" &
+           ProjectType %in% c(1, 2, 4, 8, 13))
+
 # this is useless without dates- should be moved into the app
-TotalHHExited <- Destination %>%
-  filter((str_detect(HouseholdID, fixed("s_")) |
-            (str_detect(HouseholdID, fixed("h_")) &
-               RelationshipToHoH == 1)) &
-           !is.na(ExitDate)) %>%
+# for all project types except PSH and HP
+TotalHHLeavers <- QPREEs %>%
+  filter(!is.na(ExitDate)) %>%
   group_by(ProjectName) %>%
   summarise(Leavers = n())
-
-
-PSHandHPExits <- Destination %>%
-  filter((DestinationGroup == "Permanent" | is.na(ExitDate)) &
-           (
-             str_detect(HouseholdID, fixed("s_")) |
-               (str_detect(HouseholdID, fixed("h_")) &
-                  RelationshipToHoH == 1)
-           ))
+# for PSH and HP only
+TotalHHLeaversAndStayers <- QPREEs %>%
+  group_by(ProjectName) %>%
+  summarise(LeaversAndStayers = n())
 
 #also useless without dates, should be moved into app
-PSHandHPDestinationByProject <- PSHandHPExits %>%
-  group_by(ProjectID) %>%
-  summarise()
+PermAndRetentionByProject <- PermAndRetention %>%
+  group_by(ProjectName) %>%
+  summarise(PermanentDestOrStayer = n())
+
+
+# Length of Stay ----------------------------------------------------------
+
+
+
+QPREEs <- QPREEs %>% 
+  mutate(daysinproject = difftime(ExitAdjust, EntryAdjust, units = "days"))
+
+QPREEs %>% group_by(ProjectID) %>% summarise(avg = mean(daysinproject))
+
+
 
 
 
