9270ba16f78c243dd34e8b0da10a514ab428587b
Genelle Denzin <genelle@eyedarts.net>
2019-05-27 12:15:31 -0400

    fixed the Project Types in the goals object to be numeric like everything else took out all the plotly stuff because I couldn't get it to lay out well. but then again I can't get patchwork to lay out well either, so.

--- 02_QPR_EEs.R
+++ 02_QPR_EEs.R
@@ -17,7 +17,8 @@ goals <- read_xlsx("data/Goals.xlsx")
 goals <- goals %>%
   gather(key = "ProjectType", 
          value = "Goal", 
-         -SummaryMeasure, -Measure, -Operator)
+         -SummaryMeasure, -Measure, -Operator) %>%
+  mutate(ProjectType = as.numeric(ProjectType))
 
 
 # Successful Placement ----------------------------------------------------
@@ -135,83 +136,68 @@ PermAndRetentionByProject <- PermAndRetention %>%
 
 
 # Length of Stay ----------------------------------------------------------
-
 LoSGoals <- goals %>%
   select(-Measure) %>% 
-  filter(SummaryMeasure == "Length of Stay") %>%
+  filter(SummaryMeasure == "Length of Stay" & !is.na(Goal)) %>%
   unique()
 
 LoSDetail <- QPR_EEs %>%
-  filter(ProjectType %in% c(1, 2, 8) &
-           !is.na(ExitDate)) %>%
+  filter((((!is.na(MoveInDateAdjust) &
+              ProjectType %in% c(13)) |
+             (ProjectType %in% c(1, 2, 8)) &
+             !is.na(ExitDate)
+  )) &
+    exited_between(., "01012019", "05012019")) %>%
+  left_join(LoSGoals, by = "ProjectType") %>% 
   mutate(
     ProjectType = case_when(
-      ProjectType == 1 ~ "ES",
-      ProjectType == 2 ~ "TH",
-      ProjectType %in% c(3, 9) ~ "PSH",
-      ProjectType == 4 ~ "OUT",
-      ProjectType == 8 ~ "SH",
-      ProjectType == 12 ~ "HP",
-      ProjectType == 13 ~ "RRH"
-    )
+      ProjectType == 1 ~ "Emergency Shelter",
+      ProjectType == 2 ~ "Transitional Housing",
+      ProjectType %in% c(3, 9) ~ "Permanent Supportive Housing",
+      ProjectType == 4 ~ "Street Outreach",
+      ProjectType == 8 ~ "Safe Haven",
+      ProjectType == 12 ~ "Homelessness Prevention",
+      ProjectType == 13 ~ "Rapid Rehousing"
+    ),
+    Region = paste("Homeless Planning Region", Region)
   ) %>%
-  left_join(LoSGoals, by = "ProjectType")
+  filter(Region == "Homeless Planning Region 5") # this filter needs
+  # to be here so the selection text matches the mutated data
+  
 
 LoSSummary <- LoSDetail %>%
-  group_by(brokenProjectNames, ProjectName, Region, County, ProjectType, Operator, Goal) %>%
+  group_by(brokenProjectNames,
+           ProjectName,
+           Region,
+           County,
+           ProjectType,
+           Operator,
+           Goal) %>%
   summarise(avg = as.numeric(mean(DaysinProject, na.rm = TRUE)),
-            median = as.numeric(median(DaysinProject, na.rm = TRUE))) %>%
-  arrange(ProjectType)
+            median = as.numeric(median(DaysinProject, na.rm = TRUE)))
 
-es <-
-  ggplotly(
-    ggplot(
-      LoSSummary %>% filter(Region == 5, ProjectType == "ES"),
-      aes(x = brokenProjectNames)
-    ) +
-      geom_col(aes(y = as.numeric(avg))) +
-      geom_hline(yintercept = LoSSummary$Goal[LoSSummary$ProjectType == "ES"]) +
-      theme(axis.text.x = element_text(angle = 45))
-  )
-es
-th <- ggplot(LoSSummary %>% filter(Region == 5, ProjectType == "TH"),
-             aes(x = brokenProjectNames)) +
+th <- ggplot(LoSSummary, aes(x = brokenProjectNames)) +
   ylab("Average Length of Stay") +
   xlab("") +
   ggtitle("Transitional Housing", subtitle = "date range") +
   geom_col(aes(y = as.numeric(avg)), fill = "#56B4E9") +
-  geom_hline(yintercept = LoSSummary$Goal[LoSSummary$ProjectType == "TH"]) +
-  annotate("text", x = 0.6, y = LoSSummary$Goal[LoSSummary$ProjectType == "TH"] + 1,
-           label = "CoC Goal") +
+  geom_hline(yintercept = as.integer(LoSGoals %>% 
+               filter(ProjectType == 2) %>% 
+               select(Goal))) +
+  annotate(
+    "text",
+    x = 0.65,
+    y = as.integer(LoSGoals %>% 
+                     filter(ProjectType == 2) %>% 
+                     select(Goal)) + 1,
+    label = "CoC Goal") +
+  theme_light() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 th
 
 somecolors <- c("#7156e9", "#56B4E9", "#56e98c", "#e98756", "#e9d056", "#ba56e9",
                 "#e95684")
 somemorecolors <- c('#f0f9e8','#ccebc5','#a8ddb5','#7bccc4','#4eb3d3','#2b8cbe','#08589e')
-
-plot_ly(LoSSummary %>% filter(Region == 6), type = "bar") %>%
-  add_trace(
-    x = ~ ProjectName,
-    y = ~ avg,
-    type = "bar",
-    color = ~ ProjectType
-  ) %>%
-  add_trace(x = ~ ProjectName,
-            y = ~ Goal,
-            type = "bar",
-            opacity =.5,
-            marker = list(color = "gray"),
-            name = "Goal") %>%
-  layout(
-    yaxis = list(title = "Days"),
-    xaxis = list(
-      title = "Provider",
-      categoryorder = "array",
-      categoryarray = ~ ProjectType,
-      barmode = "group"
-    )
-  )
 
 # Rapid Placement RRH -----------------------------------------------------
 
