b79c79cd29a4fc1f28a5cdce75940b5156bcca31
Genelle Denzin <genelle@eyedarts.net>
2019-07-17 22:26:35 -0400

    made small corrections to Chronic logic added SPDAT data so at the end we can use that with the Chronic data to build a Prioritization list.

--- Prioritization.R
+++ Prioritization.R
@@ -43,7 +43,7 @@ singlyChronicAtEntry <-
   mutate(ChronicAtEntry = 1)
 
 allChronicAtEntry <- 
-  left_join(
+  full_join(
     smallEnrollment,
     singlyChronicAtEntry,
     by = c(
@@ -78,11 +78,9 @@ allChronicAtEntry <- left_join(allChronicAtEntry, smallProject, by = "ProjectID"
 agedIntoChronicity <- allChronicAtEntry %>%
   filter(ProjectType %in% c(1, 8)) %>%
   mutate(
-    DaysHomelessInProject = if_else(ProjectType %in% c(1, 2, 8),
-                                    difftime(ymd(ExitAdjust),
+    DaysHomelessInProject = difftime(ymd(ExitAdjust),
                                              ymd(EntryDate),
                                              units = "days"),
-                                    0),
     DaysBetweenHomeless = difftime(ymd(EntryDate),
                                    if_else(
                                      is.na(ymd(DateToStreetESSH)), 
@@ -93,3 +91,7 @@ agedIntoChronicity <- allChronicAtEntry %>%
     ConsecutiveChronic = DaysBetweenHomeless +  DaysHomelessInProject >= 365
   ) %>%
   filter(ConsecutiveChronic == TRUE)
+
+load("images/QPR_SPDATs.RData")
+
+
