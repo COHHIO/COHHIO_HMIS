9e9a929a7454d4f873fb53033b1c8739ddde1cd5
Genelle Denzin <genelle@eyedarts.net>
2019-10-14 20:44:33 -0500

    figured out that if I don't make HoHsEntry unique, it doubles rows when I left join back to Enrollment. This is a problem even AFTER making it unique when users have created a hh with multiple HoHs AND those HoHs have different Entry Dates. but that's super rare and that kind of tangle will show in other areas too.

--- trying a thing.R
+++ trying a thing.R
@@ -2,34 +2,55 @@
 Entry Date is not equal to the HoH's Entry Date, then return the Entry Date of 
 the non-HoH as the MoveInDateAdjust."
 
-x <- Enrollment %>%
-  filter(HouseholdID == "h_141639") %>%
-  select(PersonalID, HouseholdID, EntryDate, MoveInDate, RelationshipToHoH, 
-         ProjectType, ExitAdjust) %>%
-  mutate(EntryDate = if_else(PersonalID == "108665", mdy("06012019"), EntryDate))
+library(tidyverse)
+library(janitor)
+library(lubridate)
 
-HoHsEntry <- x %>%
+load("images/COHHIOHMIS.RData")
+
+# x <- Enrollment %>%
+#   filter(HouseholdID == "h_141639") %>%
+#   select(PersonalID, HouseholdID, EntryDate, MoveInDate, RelationshipToHoH, 
+#          ProjectType, ExitAdjust) %>%
+#   mutate(EntryDate = if_else(PersonalID == "108665", mdy("06012019"), EntryDate))
+
+HoHsEntry <- Enrollment %>%
   filter(RelationshipToHoH == 1) %>%
-  select(HouseholdID, "HoHsEntry" = EntryDate)
+  select(HouseholdID, "HoHsEntry" = EntryDate) %>%
+  unique()
 
-y <- x %>%
+y <- Enrollment %>%
   left_join(HoHsEntry, by = "HouseholdID")
 
 z <- y %>%
   mutate(
     MoveInDateAdjust = case_when(
-    EntryDate < mdy("10012017") &
-      ProjectType %in% c(3, 9)
-    ~ EntryDate,
-    EntryDate != HoHsEntry &
-      RelationshipToHoH != 1 ~ EntryDate,
-    EntryDate >= mdy("10012017") &
-      ProjectType %in% c(3, 9) &
+      EntryDate < mdy("10012017") &
+        ProjectType %in% c(3, 9)
+      ~ EntryDate,
+      EntryDate != HoHsEntry &
+        ProjectType %in% c(3, 9, 13) ~ EntryDate,
+      EntryDate >= mdy("10012017") &
+        ProjectType %in% c(3, 9) &
+        ymd(EntryDate) <= ymd(MoveInDate) &
+        ymd(MoveInDate) <= ExitAdjust
+      ~ MoveInDate,
       ymd(EntryDate) <= ymd(MoveInDate) &
-      ymd(MoveInDate) <= ExitAdjust
-    ~ MoveInDate,
-    ymd(EntryDate) <= ymd(MoveInDate) &
-      ymd(MoveInDate) <= ExitAdjust &
-      ProjectType == 13 ~ MoveInDate
-  ))
-view(z)
+        ymd(MoveInDate) <= ExitAdjust &
+        ProjectType == 13 ~ MoveInDate
+    ),
+    Different = if_else(HoHsEntry != EntryDate, 1, 0)
+  ) %>% select(
+    PersonalID,
+    HouseholdID,
+    Different,
+    EntryDate,
+    HoHsEntry,
+    MoveInDate,
+    MoveInDateAdjust,
+    ExitAdjust,
+    RelationshipToHoH,
+    ProjectType
+  )
+
+count(HoHsEntry, HouseholdID) %>% view()
