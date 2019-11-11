412ac2889902b8a3ee088635c4af4996743fc952
kiadso <genelle@eyedarts.net>
2019-11-11 10:22:25 -0500

    added some file folder checks so that when non-Genelle folks are running this in my absence and there's something wrong, there will be something to tell them what is wrong.

--- 00_a_script_to_rule_them_all.R
+++ 00_a_script_to_rule_them_all.R
@@ -8,7 +8,7 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Affero General Public License for more details at
 #<https://www.gnu.org/licenses/>.
 
@@ -22,6 +22,58 @@
 
 # clearing the environment prior to running all the scripts
 rm(list = ls())
+
+# quick folder check
+
+if(length(list.files("./data", pattern = "(report_)")) > 0){
+  "There is an unnamed ReportWriter download in the data/ folder. Please either name it properly or delete it."
+} else {"You renamed all your ReportWriter files, yay!"}
+
+if(format.Date(file.info("data/Enrollment.csv")$mtime, "%F") != today()){
+  "The HUD CSV Export files are not up to date. Please be sure you unzipped the 
+  export."
+} else{"You exported and unzipped the HUD CSV Export correctly."}
+
+if(format.Date(file.info("data/RMisc.xlsx")$mtime, "%F") != today()){
+  "The RMisc.xlsx file is not up to date. Please run this ART report and 
+  overwrite the current RMisc.xlsx with the new one."
+} else{"RMisc.xlsx looks good."}
+
+if(length(list.files("./data", pattern = "(casemanagers)")) == 0){
+  "The casemanagers.zip file is missing or named incorrectly."
+} else{"Your casemanagers file looks good."}
+
+if(length(list.files("./data", pattern = "(referrals)")) == 0){
+  "The referrals.zip file is missing or named incorrectly."
+} else{"Your referrals file is ok."}
+
+if(length(list.files("./data", pattern = "(services1)")) == 0){
+  "The services1.zip file is missing or named incorrectly."
+} else{"Your services1 file is fabulous!"}
+
+if(length(list.files("./data", pattern = "(services2)")) == 0){
+  "The services2.zip file is missing or named incorrectly."
+} else{"Your services2 file is perfect!"}
+
+if(length(list.files("./data", pattern = "(offers)")) == 0){
+  "The offers.zip file is missing or named incorrectly."
+} else{"Your offers file is fine!"}
+
+if(length(list.files("./data", pattern = "(cevets)")) == 0){
+  "The cevets.zip file is missing or named incorrectly."
+} else{"Your cevets file is all good."}
+
+if(length(list.files("./data", pattern = "(providers)")) == 0){
+  "The providers.zip file is missing or named incorrectly."
+} else{"Your providers file could not be better."}
+
+if((!file.exists("./data/scoresind.zip") |
+   !file.exists("./data/scoresfam.zip") |
+   !file.exists("./data/scorestay.zip")) &
+   (!file.exists("./data/scores.csv")|
+    format.Date(file.info("data/scores.csv")$mtime, "%F") != today())){
+  "The scoresxxx.zip files are missing or out of date."
+} else{"Your scores data is finery."}
 
 source("00_get_the_CSV_things.R")
 
@@ -59,3 +111,7 @@ if (ymd(FileActualStart) <= mdy(FileStart)) {
 {
   print("Check your export Start and End Dates")
 }
+
+if(length(list.files("./data", pattern = "(odod_live_hudcsv)")) > 0){
+  "Don't forget to delete the .7z file in your /data folder. It has PII in it!"
+} else {"Your data folder looks good."}
