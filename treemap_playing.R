aab25c2a224344d82b39a5a91eee429b566c38e0
kiadso <genelle@eyedarts.net>
2019-12-03 14:38:28 -0500

    replaced all the tree map playing with circlepacking playing and it's much better but still not quite the thing

--- treemap_playing.R
+++ treemap_playing.R
@@ -1,30 +1,52 @@
-library(treemap)
-library(d3treeR)
 
-# dataset
-group <- c(rep("group-1",4),rep("group-2",2),rep("group-3",3))
-subgroup <- paste("subgroup" , c(1,2,3,4,1,2,1,2,3), sep="-")
-value <- c(13,5,22,12,11,7,3,1,23)
-data <- data.frame(group,subgroup,value)
 
-dq_summary_errors <- dq_project_summary %>%
-  filter(Type == "Error")
+dq_summary_for_a_project <- dq_project_summary %>%
+  filter(ProjectName == "Adams - Adams County Shelter for the Homeless - ES")
 
-# basic treemap
-p <- treemap(
-  dq_summary_errors %>% filter(OrganizationName == "Adams County Shelter for the Homeless, Inc."),
-  index = c("ProjectName", "Issue"),
-  vSize = "Clients",
-  type = "index",
-  palette = "Set2",
-  bg.labels = c("gray"),
-  align.labels = list(c("center", "top"),
-                      c("right", "bottom"))
-)            
+library(packcircles)
+library(ggplot2)
+library(viridis)
+library(ggiraph)
 
-# make it interactive ("rootname" becomes the title of the plot):
-inter <- d3tree2( p ,  rootname = "Data Quality Errors" )
+# Create data
+data <- dq_summary_for_a_project %>% select(Type, Issue, Clients, "text" = Guidance)
+  
+# Generate the layout
+packing <- circleProgressiveLayout(data$Clients, sizetype='area')
+data <- cbind(data, packing)
+dat.gg <- circleLayoutVertices(packing, npoints=50)
+
+# Make the plot with a few differences compared to the static version:
+p <- ggplot() +
+  geom_polygon_interactive(
+    data = dat.gg,
+    aes(
+      x,
+      y,
+      group = id,
+      fill = id,
+      tooltip = data$text[id],
+      data_id = id
+    ),
+    colour = "black",
+    alpha = 0.6
+  ) +
+  scale_fill_viridis() +
+  geom_text(
+    data = data,
+    aes(x, y,
+        label = gsub("Group_", "", Issue)),
+    size = 2,
+    color = "black"
+  ) +
+  theme_void() +
+  theme(legend.position = "none", plot.margin = unit(c(0, 0, 0, 0), "cm")) +
+  coord_equal()
+
+# Turn it interactive
+widg <- ggiraph(ggobj = p, width_svg = 7, height_svg = 7)
+# widg
 
 # save the widget
 # library(htmlwidgets)
-# saveWidget(inter, file=paste0( getwd(), "/HtmlWidget/interactiveTreemap.html"))
+# saveWidget(widg, file=paste0( getwd(), "/HtmlWidget/circular_packing_interactive.html"))
