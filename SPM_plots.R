3e463ccdfb3913dd8237436071ef4dceecff5aac
Genelle Denzin <genelle@eyedarts.net>
2019-12-25 16:44:31 -0500

    gave Prior and Current more precise yet friendly names switched to a bar chart bc the line looks ineffective without three points on the plot and I don't feel like adding another year back at this point, tho I may need to later

--- SPM_plots.R
+++ SPM_plots.R
@@ -7,30 +7,18 @@ metric_3 <- spm_3_homeless_count %>%
   filter(Type == "Unduplicated Total Sheltered Homeless Persons") %>%
   pivot_longer(cols = c("Prior", "Current")) %>%
   mutate(Year = case_when(
-    name == "Prior" ~ ymd(spm_prior_end_date),
-    name == "Current" ~ ymd(spm_current_end_date)
+    name == "Prior" ~ paste(format.Date(spm_prior_start_date, "%b %Y"), "to",
+                            format.Date(spm_prior_end_date - days(1), "%b %Y")),
+    name == "Current" ~ paste(format.Date(spm_current_start_date, "%b %Y"), "to",
+                              format.Date(spm_current_end_date - days(1), "%b %Y"))
   )) %>%
   select("Measure" = Type, "LiterallyHomeless" = value, Year)
 
 ggplot(metric_3) +
-  geom_line(aes(Year, LiterallyHomeless)) +
-  geom_point(aes(Year, LiterallyHomeless)) +
-  ylim(0, 15000) +
+  geom_col(aes(Year, LiterallyHomeless)) +
   labs(title =
-         "Metric 3: Homeless Clients Served in Shelter or Transitional Housing",
-       caption = "These numbers do not include clients who may have experienced
-       unsheltered homelessness or who were at non-HMIS-participating 
-       emergency shelters.") +
+         "Metric 3: Homeless Clients Served in Shelter or Transitional Housing") +
   xlab("") +
   ylab("Homeless Clients in HMIS") +
-  scale_x_date(
-    date_labels = "%Y",
-    breaks = c(spm_prior_end_date, spm_current_end_date),
-    minor_breaks = NULL,
-    limits = c(
-      spm_prior_end_date - months(4),
-      spm_current_end_date + months(4)
-    )
-  ) +
   theme_bw() 
 
