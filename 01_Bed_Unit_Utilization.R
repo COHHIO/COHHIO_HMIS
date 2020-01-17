309d76f2f99498872f60c9b051e2e1083fcd21e1
Genelle Denzin <genelle@eyedarts.net>
2020-01-17 08:49:32 -0500

    commenting

--- 01_Bed_Unit_Utilization.R
+++ 01_Bed_Unit_Utilization.R
@@ -8,9 +8,9 @@
 # 
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Affero General Public License for more details at 
-#<https://www.gnu.org/licenses/>.
+# <https://www.gnu.org/licenses/>.
 
 library(tidyverse)
 library(lubridate)
