c575f4f2528d17f701b4c2b4971c6ad6310bd526
kiadso <genelle@eyedarts.net>
2020-02-13 11:04:19 -0500

    wrote a more complete script for the meeting

--- PE_meeting_prep.R
+++ PE_meeting_prep.R
@@ -1,13 +1,38 @@
+library(tidyverse)
+library(lubridate)
 
+load("images/Data_Quality.RData")
+load("images/ProjectEvaluation.RData")
 
+# VI-SPDATs missing going into PH -----------------------------------------
 
 vispdats_entering_ph <- dq_2019 %>%
   right_join(pe_coc_funded, by = c("ProjectID", "ProjectType", "ProjectName")) %>%
   filter(Issue == "Non-Veteran Non-DV HoHs Entering PH without SPDAT") %>%
-  select(ProjectName, PersonalID) %>% group_by(ProjectName) %>% summarise(n())
+  select(ProjectName, PersonalID) %>% 
+  group_by(ProjectName) %>% 
+  summarise(n()) %>%
+  view()
 
+# looked up some clients in Miami Family Abuse and the clients do not appear to 
+# be fleeing DV, acc. to the assessment data. Same goes for RROhio.
+# when we get calls/questions about this DQ item we find:
+# sometimes they're expired
+# most the time they really are missing
 
-# dq_flags (11/93) have flags
+# DQ_flag Point Forfeits --------------------------------------------------
 
+dq_flags 
+#(11/93) have flags
+
+# Compare HoHs entering to Adults entering --------------------------------
+
+summary(summary_pe_homeless_history_index)
+summary(summary_pe_homeless_history_index_test)
+# adults: SLIGHTLY lower Means than HoHs
+
+summary(summary_pe_long_term_homeless)
+summary(summary_pe_long_term_homeless_test)
+# adults: a little higher Means than HoHs
 
 
