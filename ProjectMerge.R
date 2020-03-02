387d8ed0aacbf25dbda20a99d1947696cde72468
kiadso <genelle@eyedarts.net>
2020-03-02 12:28:34 -0500

    added the rest of the consolidations

--- ProjectMerge.R
+++ ProjectMerge.R
@@ -1,19 +1,38 @@
 
 
-
-
 load("images/COHHIOHMIS.RData")
 
-x <- Project %>%
-  filter(ProjectID %in% c(718, 719, 721, 1353, 1354)) %>%
+# this is based on the ReportWriter reports we wrote for users to combine
+# HMIS data. Confirmed by EM.
+
+y <- Project %>%
+  filter(ProjectID %in% c(718, 719, 721, 
+                          1353, 1354, 
+                          746, 747, 
+                          1774, 15,
+                          737, 738, 739,
+                          548, 763, 764, 774,
+                          1323, 208)) %>%
   mutate(
     AltProjectID = case_when(
       ProjectID %in% c(718, 719, 721) ~ 3000,
-      ProjectID %in% c(1353, 1354) ~ 3001
+      ProjectID %in% c(1353, 1354) ~ 3001,
+      ProjectID %in% c(746, 747) ~ 3002,
+      ProjectID %in% c(1774, 15) ~ 3003,
+      ProjectID %in% c(737, 738, 739) ~ 3004,
+      ProjectID %in% c(548, 763, 764, 774) ~ 3005,
+      ProjectID %in% c(1323, 208) ~ 3006
     ),
     AltProjectName = case_when(
       ProjectID %in% c(718, 719, 721) ~ "Butler SPC Combined",
-      ProjectID %in% c(1353, 1354) ~ "Clark SPC Combined"
+      ProjectID %in% c(1353, 1354) ~ "Clark SPC Combined",
+      ProjectID %in% c(746, 747) ~ "Jefferson PSH Combined",
+      ProjectID %in% c(1774, 15) ~ "GLCAP PSH Combined",
+      ProjectID %in% c(737, 738, 739) ~ "Lake PSH Combined",
+      ProjectID %in% c(548, 763, 764, 774) ~ "Trumbull PSH Combined",
+      ProjectID %in% c(1323, 208) ~ "Warren PSH Combined"
     )
   ) %>%
-  select(ProjectID, AltProjectID, ProjectName, AltProjectName)
+  select(ProjectID, ProjectName, AltProjectID, AltProjectName)
+
+
