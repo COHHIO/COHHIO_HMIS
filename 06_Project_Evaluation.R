ca2f1422cbd3d981b884ac616f129fbb8672db26
kiadso <genelle@eyedarts.net>
2020-03-04 16:40:50 -0500

    removed LoS from RRH

--- 06_Project_Evaluation.R
+++ 06_Project_Evaluation.R
@@ -1077,18 +1077,17 @@ summary_pe_length_of_stay <- pe_length_of_stay %>%
   mutate(
     Structure = case_when(
       ProjectType == 2 ~ "200_280_10",
-      ProjectType == 8 ~ "260_340_10",
-      ProjectType == 13 ~ "150_210_10"
+      ProjectType == 8 ~ "260_340_10"
     ),
     AverageLoSPoints = case_when(
       ClientsMovedInLeavers == 0 &
         ProjectType != 3 ~ 10,
       TRUE ~ pe_score(Structure, AverageDays)
     ),
-    AverageLoSPossible = if_else(ProjectType %in% c(2, 8, 13), 10, NULL),
+    AverageLoSPossible = if_else(ProjectType %in% c(2, 8), 10, NULL),
     AverageLoSDQ = case_when(
-      General_DQ == 1 & ProjectType %in% c(2, 8 ,13) ~ 1,
-      General_DQ == 0 & ProjectType %in% c(2, 8 ,13) ~ 0),
+      General_DQ == 1 & ProjectType %in% c(2, 8) ~ 1,
+      General_DQ == 0 & ProjectType %in% c(2, 8) ~ 0),
     AverageLoSPoints = case_when(
       AverageLoSDQ == 1 ~ 0, 
       AverageLoSDQ == 0 | is.na(AverageLoSDQ) ~ AverageLoSPoints),
