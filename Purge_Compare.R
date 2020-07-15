a4ccff6f22ecfe83bfad8f3bd1a71a4211e427f7
Genelle Denzin <genelledenzin@cohhio.org>
2020-07-15 10:22:42 -0500

    working on testing data archive

--- Purge_Compare.R
+++ Purge_Compare.R
@@ -5,13 +5,13 @@ library(knitr)
 library(here)
 library(readxl)
 
-cutoff_date <- mdy("01012013")
+cutoff_date <- mdy("7/1/2013")
 
-highest_clientid_on_demo <- 244417
+highest_clientid_on_demo <- 244010
 
-highest_ee_on_demo <- 395799
+highest_ee_on_demo <- 394982
 
-highest_serviceid_on_demo <- 549938
+highest_serviceid_on_demo <- 548448
 
 live_services <- read_xlsx(here("data/LIVEARTCompare.xlsx"), sheet = 2) %>%
   filter(ServiceID <= highest_serviceid_on_demo)
@@ -22,18 +22,7 @@ live_ees <- read_xlsx(here("data/LIVEARTCompare.xlsx"), sheet = 1) %>%
 demo_services <- read_xlsx(here("data/DEMOARTCompare.xlsx"), sheet = 2)
 demo_ees <- read_xlsx(here("data/DEMOARTCompare.xlsx"), sheet = 1)
 
-ees_missing_from_demo <- live_ees %>% anti_join(demo_ees)
 
-services_missing_from_demo <- live_services %>% anti_join(demo_services)
-
-
-
-# Check that demo has no clients who exited earlier than the cutoff date.
-
-
-if_else(demo_ees %>% filter(ymd(Exit) > cutoff_date) %>% nrow() > 0,
-   "at least one client on the demo site exited after the cutoff date (ng)",
-   "no client exited after the cutoff date (ok)")
 
 
 
--- tests/testthat/test_Purge_Compare.R
+++ tests/testthat/test_Purge_Compare.R
@@ -1,6 +1,16 @@
 library(testthat)
 
+source(here("Purge_Compare.R"))
 
+ees_missing_from_demo <- live_ees %>% anti_join(demo_ees)
+
+services_missing_from_demo <- live_services %>% anti_join(demo_services)
+
+# Check that demo has no clients who exited earlier than the cutoff date.
+
+if_else(demo_ees %>% filter(ymd(Exit) > cutoff_date) %>% nrow() > 0,
+        "at least one client on the demo site exited after the cutoff date (ng)",
+        "no client exited after the cutoff date (ok)")
 
 
 
