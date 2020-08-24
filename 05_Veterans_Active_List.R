c977234b767648455c21be69b0f46113a13e4aea
kiadso <genelle@eyedarts.net>
2020-08-24 16:06:13 -0400

    mostly recreating the report in ART since it is good and has the right logic trying to use days(90) since that's an option in R unlike in ART where I had to use the equivalent of months(3)

--- 05_Veterans_Active_List.R
+++ 05_Veterans_Active_List.R
@@ -21,14 +21,17 @@ load("images/COHHIOHMIS.RData")
 
 vet_projects <- Project %>%
   filter(ProjectType %in% c(1, 2, 3, 4, 8, 9, 13) &
-           operating_between(., format(today() - months(3), "%m%d%Y"),
+           operating_between(., format(today() - days(90), "%m%d%Y"),
                                format(today(), "%m%d%Y")))
 
-# Responsible Providers ---------------------------------------------------
-
-
-
 # Get all veterans and associated hh members ------------------------------
+
+responsible_providers <- ServiceAreas %>%
+  select(County, SSVFServiceArea) 
+
+bos_counties <- ServiceAreas %>%
+  filter(CoC == "OH-507 Balance of State") %>%
+  pull(County)
 
 vet_ees <- Enrollment %>%
   left_join(Client %>% select(PersonalID, VeteranStatus), by = "PersonalID") %>%
@@ -38,13 +41,30 @@ vet_ees <- Enrollment %>%
   ungroup() %>%
   filter(VetCount > 0) %>%
   left_join(Enrollment, by = "HouseholdID") %>%
-  left_join(Client %>% select(PersonalID, VeteranStatus), by = "PersonalID")
+  left_join(Client %>% select(PersonalID, VeteranStatus), by = "PersonalID") %>%
+  filter((CountyServed %in% c(bos_counties) | is.na(CountyServed)) &
+           !ProjectID %in% c(1282)) %>%
+  select(1, 3:15, 17, 51, 67, 73, 75:90)
 
 # Currently in PSH/RRH ----------------------------------------------------
 
+# RRH PSH stays with no Exit but a valid Move-In Date
+
+currently_housed_in_psh_rrh <- vet_ees %>%
+  filter(stayed_between(., start = format(today(), "%m%d%Y"), 
+                        end = format(today(), "%m%d%Y")) &
+           ProjectType %in% c(3, 9, 13)) %>%
+  pull(HouseholdID)
 
 # Active List -------------------------------------------------------------
 
+# stayers & people who exited in the past 90 days to a temp destination
+
+active_list <- vet_ees %>%
+  filter(!HouseholdID %in% c(currently_housed_in_psh_rrh) &
+           (is.na(ExitDate) |
+              (!Destination %in% c(3, 10, 11, 19:23, 28, 31, 33, 34, 36) &
+                 ymd(ExitDate) >= today() - days(90))))
 
 # Currently Homeless Vets -------------------------------------------------
 
