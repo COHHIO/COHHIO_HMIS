a976871594204527c7ae57aef9e216ba1452e6f7
kiadso <genelledenzin@cohhio.org>
2020-09-15 08:13:52 -0400

    reviewed some data the CDC sent me in pursuit of a Prevention tool that gets at geography

---
+++ Experiments/CDC_SVI_data.R
@@ -0,0 +1,35 @@
+# COHHIO_HMIS
+# Copyright (C) 2019  Coalition on Homelessness and Housing in Ohio (COHHIO)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details at
+# <https://www.gnu.org/licenses/>.
+
+library(tidyverse)
+library(lubridate)
+library(scales)
+library(here)
+
+cvi <- read_csv(here("Ohio/Ohio_COUNTY.csv")) %>%
+  select(COUNTY,
+         "SocioeconomicStatus" = RPL_THEME1,
+         "HouseholdCompositionDisability" = RPL_THEME2,
+         "MinorityStatusLanguage" = RPL_THEME3,
+         "HousingTypeTransportation" = RPL_THEME4,
+         RPL_THEMES) %>%
+  mutate(
+    SocioeconomicStatus_pct = percent(SocioeconomicStatus),
+    HouseholdCompositionDisability_pct = percent(HouseholdCompositionDisability),
+    MinorityStatusLanguage_pct = percent(MinorityStatusLanguage),
+    HousingTypeTransportation_pct = percent(HousingTypeTransportation),
+    RPL_THEMES_pct = percent(RPL_THEMES)
+  )
+  
+
