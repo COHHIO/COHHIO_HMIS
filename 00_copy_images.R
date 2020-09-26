65d7afd76823a62f46e8e8e11707cca0638d75ef
Genelle Denzin <7561522+kiadso@users.noreply.github.com>
2020-09-26 17:41:14 -0400

    still working on the copy images solution because the current code leaves the images at 0 bytes for some reason? Almost like it's moving the files instead of copying them? Anyway, trying different solutions.

--- 00_copy_images.R
+++ 00_copy_images.R
@@ -1,22 +1,51 @@
-.images_to_copy <- c(
-  "Utilization"
-  , "QPR_SPDATs"
-  , "QPR_EEs"
-  , "Veterans"
-  , "Data_Quality"
-  , "SPM_data"
-  , "ProjectEvaluation"
-  , "Active_List"
+# .images_to_copy <- c(
+#   "Utilization"
+#   , "QPR_SPDATs"
+#   , "QPR_EEs"
+#   , "Veterans"
+#   , "Data_Quality"
+#   , "SPM_data"
+#   , "ProjectEvaluation"
+#   , "Active_List"
+# )
+# 
+# .dir <- c("../Rminor/data", "../Rminor_elevated/data", "../COHHIO_HMIS/images")
+# 
+# purrr::walk(.dir, ~ {
+#   .d <- .x
+#   if (!dir.exists(.d)) {
+#     dir.create(.d)
+#   }
+#   purrr::walk(paste0("images/", .images_to_copy, ".RData"), ~ {
+#     file.copy(.x, overwrite = TRUE, fs::path(.d, basename(.x)))
+#   })
+# }) 
+
+
+directory_where_all_your_projects_live <-
+  "C:\\Users\\HMIS\\Documents\\R\\"
+
+project_and_folder_that_contains_image <- "COHHIO_HMIS\\images\\"
+
+image_filename <- c(
+  "Data_Quality.RData",
+  "ProjectEvaluation.RData",
+  "Utilization.RData",
+  "QPR_SPDATs.RData",
+  "QPR_EEs.RData",
+  "Veterans.RData",
+  "SPM_data.RData",
+  "Active_List.RData"
 )
 
-.dir <- c("../Rminor/data", "../Rminor_elevated/data")
+project_and_folder_you_wish_had_the_image <- "Rminor\\data\\"
 
-purrr::walk(.dir, ~ {
-  .d <- .x
-  if (!dir.exists(.d)) {
-    dir.create(.d)
-  }
-  purrr::walk(paste0("images/", .images_to_copy, ".RData"), ~ {
-    file.copy(.x, overwrite = TRUE, fs::path(.d, basename(.x)))
-  })
-}) 
+file.link(from = paste0(
+  directory_where_all_your_projects_live,
+  project_and_folder_that_contains_image, 
+  image_filename),
+  to = paste0(
+    directory_where_all_your_projects_live,
+    project_and_folder_you_wish_had_the_image, 
+    image_filename
+  ))
