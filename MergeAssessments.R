b9158a42f2bfa34dcbce112ceada1f7693a3c34d
Genelle Denzin <genelledenzin@cohhio.org>
2020-10-15 13:39:57 -0400

    now with a clearer vision of what we're wanting to know, ditched some exploratory code and kept just what we need to run against NV for comment

--- MergeAssessments.R
+++ MergeAssessments.R
@@ -17,135 +17,115 @@ library(readxl)
 
 # Youngstown --------------------------------------------------------------
 
-assessments_yo <- read_xlsx("random_data/yo_assessments.xlsx",
-                                 sheet = 1)
+raw_assessments_yo <- read_xlsx("random_data/yo_assessments.xlsx",
+                                sheet = 1)
 
-questions_yo <- read_xlsx("random_data/yo_assessments.xlsx",
-                          sheet = 2)
+raw_questions_yo <- read_xlsx("random_data/yo_assessments.xlsx",
+                              sheet = 2)
 
-subs_yo <- read_xlsx("random_data/yo_assessments.xlsx",
-                     sheet = 3)
-sub_questions_yo <- read_xlsx("random_data/yo_assessments.xlsx",
-                              sheet = 4)
+raw_subs_yo <- read_xlsx("random_data/yo_assessments.xlsx",
+                         sheet = 3)
+raw_sub_questions_yo <- read_xlsx("random_data/yo_assessments.xlsx",
+                                  sheet = 4)
 
-assessment_question_link_yo <- read_xlsx("random_data/yo_assessments.xlsx",
-                                         sheet = 5)
+raw_assessment_question_link_yo <-
+  read_xlsx("random_data/yo_assessments.xlsx",
+            sheet = 5)
 
-assessment_sub_question_link_yo <- read_xlsx("random_data/yo_assessments.xlsx",
-                                             sheet = 6)
+raw_assessment_sub_question_link_yo <-
+  read_xlsx("random_data/yo_assessments.xlsx",
+            sheet = 6)
 
-picklists_yo <- read_xlsx("random_data/yo_assessments.xlsx",
-                          sheet = 7)
+raw_picklists_yo <- read_xlsx("random_data/yo_assessments.xlsx",
+                              sheet = 7)
 
 # Balance of State --------------------------------------------------------
 
-assessments_bos <- read_xlsx("random_data/bos_assessments.xlsx",
+raw_assessments_bos <- read_xlsx("random_data/bos_assessments.xlsx",
                                  sheet = 1)
 
-questions_bos <- read_xlsx("random_data/bos_assessments.xlsx",
-                          sheet = 2)
+raw_questions_bos <- read_xlsx("random_data/bos_assessments.xlsx",
+                               sheet = 2)
 
-subs_bos <- read_xlsx("random_data/bos_assessments.xlsx",
-                     sheet = 3)
-sub_questions_bos <- read_xlsx("random_data/bos_assessments.xlsx",
-                              sheet = 4)
+raw_subs_bos <- read_xlsx("random_data/bos_assessments.xlsx",
+                          sheet = 3)
+raw_sub_questions_bos <-
+  read_xlsx("random_data/bos_assessments.xlsx",
+            sheet = 4)
 
-assessment_question_link_bos <- read_xlsx("random_data/bos_assessments.xlsx",
-                                         sheet = 5)
+raw_assessment_question_link_bos <-
+  read_xlsx("random_data/bos_assessments.xlsx",
+            sheet = 5)
 
-assessment_sub_question_link_bos <- read_xlsx("random_data/bos_assessments.xlsx",
-                                             sheet = 6)
+raw_assessment_sub_question_link_bos <-
+  read_xlsx("random_data/bos_assessments.xlsx",
+            sheet = 6)
 
-picklists_bos <- read_xlsx("random_data/bos_assessments.xlsx",
-                          sheet = 7)
+raw_picklists_bos <- read_xlsx("random_data/bos_assessments.xlsx",
+                               sheet = 7)
 
 # Question Differences ----------------------------------------------------
 
-question_names_bos <- questions_bos$QuestionComputerName 
-  
-question_names_yo <- questions_yo$QuestionComputerName
-
-questionsysnames_not_on_yo <- setdiff(questions_bos$QuestionComputerName, 
-                                      questions_yo$QuestionComputerName)
-
-questionsysnames_not_on_bos <- setdiff(questions_yo$QuestionComputerName, 
-                                       questions_bos$QuestionComputerName)
-
-same_questions <- intersect(questions_bos, questions_yo)
-
+questionsysnames_not_on_bos <-
+  data.frame(
+    "QuestionComputerName" = setdiff(
+      raw_questions_yo$QuestionComputerName,
+      raw_questions_bos$QuestionComputerName
+    )
+  ) %>%
+  left_join(raw_questions_yo, by = "QuestionComputerName")
 
 # Picklist Differences ----------------------------------------------------
 
-picklist_names_bos <- picklists_bos$PicklistName 
-
-picklist_names_yo <- picklists_yo$PicklistName
-
-picklist_names_not_on_yo <- setdiff(picklist_names_bos, picklist_names_yo)
-
-picklist_names_not_on_bos <- setdiff(picklist_names_yo, picklist_names_bos)
-
-same_picklists <- intersect(picklists_bos, picklists_yo)
-
-same_picklist_names <- intersect(picklist_names_bos, picklist_names_yo)
+picklist_names_not_on_bos <-
+  data.frame("PicklistName" = setdiff(
+    raw_picklists_yo$PicklistName,
+    raw_picklists_bos$PicklistName
+  )) %>%
+  left_join(raw_picklists_yo, by = "PicklistName")
 
 # Assessment Differences --------------------------------------------------
 
-assessment_names_bos <- assessments_bos$AssessmentComputerName 
+assessment_names_not_on_bos <-
+  setdiff(
+    raw_assessments_yo$AssessmentComputerName,
+    raw_assessments_bos$AssessmentComputerName
+  )
 
-assessment_names_yo <- assessments_yo$AssessmentComputerName
-
-assessment_names_not_on_yo <- setdiff(assessment_names_bos, assessment_names_yo)
-
-assessment_names_not_on_bos <- setdiff(assessment_names_yo, assessment_names_bos)
-
-same_assessments <- intersect(assessments_bos, assessments_yo)
-
-same_assessment_names <- intersect(assessment_names_bos, assessment_names_yo)
-
-assessment_names_not_on_bos <- 
-  data.frame("AssessmentComputerName" = assessment_names_not_on_bos)
-
-subassessment_qs_not_on_bos <-  assessment_names_not_on_bos %>%
-  left_join(assessments_yo, by = "AssessmentComputerName")
+assessment_names_not_on_bos <-
+  data.frame("AssessmentComputerName" = assessment_names_not_on_bos) %>%
+  left_join(raw_assessments_yo, by = "AssessmentComputerName")
 
 # Subassessments ----------------------------------------------------------
-subs_not_on_bos <- data.frame(
-  "SubComputerName" = setdiff(
-    subs_yo$SubComputerName,
-    subs_bos$SubComputerName
-  )
-)
+subs_not_on_bos <- data.frame("SubComputerName" = setdiff(raw_subs_yo$SubComputerName,
+                                                          raw_subs_bos$SubComputerName))
 
 subassessments_not_on_bos <-  subs_not_on_bos %>%
-  left_join(subs_yo, by = "SubComputerName")
+  left_join(raw_subs_yo, by = "SubComputerName")
 
 
 # Sub Questions -----------------------------------------------------------
 
 sub_qs_not_on_bos <- data.frame(
   "SubQuestionComputerName" = setdiff(
-    sub_questions_yo$SubQuestionComputerName,
-    sub_questions_bos$SubQuestionComputerName
+    raw_sub_questions_yo$SubQuestionComputerName,
+    raw_sub_questions_bos$SubQuestionComputerName
   )
 )
 
 subassessment_qs_not_on_bos <-  sub_qs_not_on_bos %>%
-  left_join(sub_questions_yo, by = "SubQuestionComputerName")
+  left_join(raw_sub_questions_yo, by = "SubQuestionComputerName")
 
 # To Be Discussed ---------------------------------------------------------
 
-write_it_out <- function(object) {
-  write_csv(object, paste0(str_remove_all(!!object, "_"), ".csv"))
-  cat("done")
-}
+datasets_to_examine <- list(
+  "assessments" = assessment_names_not_on_bos,   
+  "questions" = questionsysnames_not_on_bos,
+  "subs" = subassessments_not_on_bos,
+  "subqs" = subassessment_qs_not_on_bos,
+  "picklists" = picklist_names_not_on_bos
+)
 
-write_it_out(questionsysnames_not_on_bos)
-write_it_out(picklist_names_not_on_bos)
-write_it_out(assessment_names_not_on_bos)
-write_it_out(subassessments_not_on_bos)
-
-write_csv(subassessments_not_on_bos, 
-          paste0(str_remove_all(subassessments_not_on_bos, "_"), ".csv"))
-
+write.xlsx(datasets_to_examine, file = "YoungstownDifferences.xlsx")
 
 
