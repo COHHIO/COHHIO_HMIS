93d3eb2de17bade8a47ad7425ece25ba8ef731d2
Genelle Denzin <genelledenzin@cohhio.org>
2020-11-21 10:20:52 -0500

    excluding Mah CE project from the "no ee's on an AP" logic

--- 04_DataQuality.R
+++ 04_DataQuality.R
@@ -2361,7 +2361,7 @@ check_eligibility <- served_in_date_range %>%
     # AP entering project stays -----------------------------------------------
     
     aps_with_ees <- served_in_date_range %>%
-      filter(ProjectType == 14) %>%
+      filter(ProjectType == 14 & ProjectID != 2372) %>% # not incl Mah CE
       mutate(
         Issue = "Access Point with Entry Exits",
         Type = "High Priority",
