0cffb3f8dd81ca4902dffd87162fcb9f799fd4ba
Genelle Denzin <genelledenzin@cohhio.org>
2020-11-26 15:14:36 -0500

    finding Mahoning users

---
+++ Reports/Provider_Admin/findMahoningUsersForRmeInvites.R
@@ -0,0 +1,27 @@
+# COHHIO_HMIS
+# Copyright (C) 2019  Coalition on Homelessness and Housing in Ohio (COHHIO)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details at
+# <https://www.gnu.org/licenses/>.
+
+library(tidyverse)
+library(readxl)
+
+users <- read_xlsx("data/RMisc2.xlsx", sheet = 2)
+
+
+mahoning_users <- users %>%
+  filter(UserID > 1946 & UserID < 2167 & UserActive == "Yes" &
+           DefaultProvider != "Bowman Systems, LLC(0)")
+
+mahoning_emails <- mahoning_users[c("UserName", "UserEmail")]  
+  
+  
