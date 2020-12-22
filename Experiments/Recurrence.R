3279cef0ae157a46b5266f4be5e393aefe18e104
Genelle Denzin <genelledenzin@cohhio.org>
2020-12-22 17:21:05 -0500

    taking an other crack at coding recurrence using GB's code! only got to line 23 with aligning everything to my stuff

---
+++ Experiments/Recurrence.R
@@ -0,0 +1,78 @@
+# MIT License
+
+# Copyright (c) 2020 gwenbeebe
+
+library(tidyverse)
+library(lubridate)
+library(dplyr)
+
+load("images/cohorts.RData")
+
+##  set up definitions and initial dataframe, adjust as needed
+all_program_types <- c(1:4, 6:14)   
+
+return_project_types <- c(2, 3, 9, 10, 1, 4, 13, 8, 14)
+
+housing_program_types <- c(2, 3, 9, 10, 13)
+
+ph_program_types <- c(3, 9, 10, 13)
+
+df_for_returns <- co_clients_served %>%
+  filter(ProjectType %in% return_project_types) %>%
+  select(PersonalID, EnrollmentID, EntryDate, ExitDate, ProjectType, Destination) %>%
+  mutate(two_weeks_after_exit = if_else(!is.na(ExitDate), ExitDate + ddays(14), NULL))
+
+
+##  find all exits from TH or PH programs
+housing_exits <- df_for_returns %>%
+  filter(ProgramType %in% housing_program_types,
+         !is.na(ExitDate)) %>%
+  setNames(paste("H_Ex", colnames(df_for_returns), sep = "_"))
+
+##  find all entries to PH programs
+ph_enrollments <- df_for_returns %>%
+  filter(ProgramType %in% ph_program_types) %>%
+  setNames(paste("PH_En", colnames(df_for_returns), sep = "_")) %>%
+  select(-PH_En_two_weeks_after_exit)
+
+##  identify all PH enrollments within 14 days of a TH or PH exit
+excluded_PH_entries <- ph_enrollments %>%
+  left_join(housing_exits, by = c("PH_En_ClientID" = "H_Ex_ClientID")) %>%
+  filter(PH_En_EnrollDate >= H_Ex_ExitDate &
+           PH_En_EnrollDate <= H_Ex_two_weeks_after_exit) %>%
+  select(PH_En_EnrollID) %>%
+  distinct()
+
+##  remove enrollments identified above from enrollments used to flag returns
+returning_entries <- df_for_returns %>%
+  anti_join(excluded_PH_entries, by = c("EnrollID" = "PH_En_EnrollID")) %>%
+  setNames(paste("R_En", colnames(df_for_returns), sep = "_"))
+
+##  get all enrollments with permanent exits
+permanent_exits <- df_for_returns %>%
+  filter(ExitDestination %in% permanent_destinations) %>%
+  setNames(paste("PEx", colnames(df_for_returns), sep = "_")) %>%
+  mutate(two_years_after_exit = PEx_ExitDate + dyears(2))
+
+## create flag for all enrollments with a qualifying returning entry
+return_flags <- permanent_exits %>%
+  left_join(returning_entries, by = c("PEx_ClientID" = "R_En_ClientID")) %>%
+  group_by(PEx_EnrollID) %>%
+  mutate(return_flag = 
+           if_else(
+             (R_En_ProgramType %in% housing_program_types &
+                R_En_EnrollDate >= PEx_two_weeks_after_exit &
+                R_En_EnrollDate <= two_years_after_exit) |
+               (!R_En_ProgramType %in% housing_program_types &
+                  R_En_EnrollDate >= PEx_ExitDate &
+                  R_En_EnrollDate <= two_years_after_exit),
+             1, 0
+           ),
+         return_flag = if_else(is.na(max(return_flag)), 0, max(return_flag))) %>%
+  ungroup() %>%
+  select(PEx_EnrollID, return_flag) %>%
+  distinct()
+
+rm(list = ls()[!(ls() %in% c("return_flags"))])
+save.image("images/return_flags.RData")
+
