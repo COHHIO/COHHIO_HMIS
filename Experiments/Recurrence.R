a18a6a4d4f23df265193f9ffe075fbded5a3ed98
Genelle Denzin <genelledenzin@cohhio.org>
2020-12-23 13:33:44 -0500

    name changes only, some to match HUD CSV Export, some just bc I'm easily confused

--- Experiments/Recurrence.R
+++ Experiments/Recurrence.R
@@ -8,6 +8,8 @@ library(dplyr)
 
 load("images/cohorts.RData")
 
+ReportEnd <- mdy("09302020")
+
 ##  set up definitions and initial dataframe, adjust as needed
 all_program_types <- c(1:4, 6:14)   
 
@@ -25,52 +27,53 @@ df_for_returns <- co_clients_served %>%
 
 ##  find all exits from TH or PH programs
 housing_exits <- df_for_returns %>%
-  filter(ProgramType %in% housing_program_types,
+  filter(ProjectType %in% housing_program_types,
          !is.na(ExitDate)) %>%
-  setNames(paste("H_Ex", colnames(df_for_returns), sep = "_"))
+  setNames(paste("Exited_PHTH", colnames(df_for_returns), sep = "_"))
 
 ##  find all entries to PH programs
 ph_enrollments <- df_for_returns %>%
-  filter(ProgramType %in% ph_program_types) %>%
-  setNames(paste("PH_En", colnames(df_for_returns), sep = "_")) %>%
-  select(-PH_En_two_weeks_after_exit)
+  filter(ProjectType %in% ph_program_types) %>%
+  setNames(paste("Entered_PH", colnames(df_for_returns), sep = "_")) %>%
+  select(-Entered_PH_two_weeks_after_exit)
 
-##  identify all PH enrollments within 14 days of a TH or PH exit
+##  identify all PH enrollments within 14 days of a TH or PH exit 
+## (wouldn't we also want to get exits from ESs and SHs?)
 excluded_PH_entries <- ph_enrollments %>%
-  left_join(housing_exits, by = c("PH_En_ClientID" = "H_Ex_ClientID")) %>%
-  filter(PH_En_EnrollDate >= H_Ex_ExitDate &
-           PH_En_EnrollDate <= H_Ex_two_weeks_after_exit) %>%
-  select(PH_En_EnrollID) %>%
+  left_join(housing_exits, by = c("Entered_PH_PersonalID" = "Exited_PHTH_PersonalID")) %>%
+  filter(Entered_PH_EntryDate >= Exited_PHTH_ExitDate &
+           Entered_PH_EntryDate <= Exited_PHTH_two_weeks_after_exit) %>%
+  select(Entered_PH_EnrollmentID) %>%
   distinct()
 
 ##  remove enrollments identified above from enrollments used to flag returns
 returning_entries <- df_for_returns %>%
-  anti_join(excluded_PH_entries, by = c("EnrollID" = "PH_En_EnrollID")) %>%
-  setNames(paste("R_En", colnames(df_for_returns), sep = "_"))
+  anti_join(excluded_PH_entries, by = c("EnrollmentID" = "Entered_PH_EnrollmentID")) %>%
+  setNames(paste("Returning_Entries", colnames(df_for_returns), sep = "_"))
 
 ##  get all enrollments with permanent exits
 permanent_exits <- df_for_returns %>%
-  filter(ExitDestination %in% permanent_destinations) %>%
-  setNames(paste("PEx", colnames(df_for_returns), sep = "_")) %>%
-  mutate(two_years_after_exit = PEx_ExitDate + dyears(2))
+  filter(Destination %in% c(perm_destinations)) %>%
+  setNames(paste("Perm_Exits", colnames(df_for_returns), sep = "_")) %>%
+  mutate(two_years_after_exit = Perm_Exits_ExitDate + dyears(2))
 
 ## create flag for all enrollments with a qualifying returning entry
 return_flags <- permanent_exits %>%
-  left_join(returning_entries, by = c("PEx_ClientID" = "R_En_ClientID")) %>%
-  group_by(PEx_EnrollID) %>%
+  left_join(returning_entries, by = c("Perm_Exits_PersonalID" = "Returning_Entries_PersonalID")) %>%
+  group_by(Perm_Exits_EnrollmentID) %>%
   mutate(return_flag = 
            if_else(
-             (R_En_ProgramType %in% housing_program_types &
-                R_En_EnrollDate >= PEx_two_weeks_after_exit &
-                R_En_EnrollDate <= two_years_after_exit) |
-               (!R_En_ProgramType %in% housing_program_types &
-                  R_En_EnrollDate >= PEx_ExitDate &
-                  R_En_EnrollDate <= two_years_after_exit),
+             (Returning_Entries_ProjectType %in% housing_program_types &
+                Returning_Entries_EntryDate >= Perm_Exits_two_weeks_after_exit &
+                Returning_Entries_EntryDate <= two_years_after_exit) |
+               (!Returning_Entries_ProjectType %in% housing_program_types &
+                  Returning_Entries_EntryDate >= Perm_Exits_ExitDate &
+                  Returning_Entries_EntryDate <= two_years_after_exit),
              1, 0
            ),
          return_flag = if_else(is.na(max(return_flag)), 0, max(return_flag))) %>%
   ungroup() %>%
-  select(PEx_EnrollID, return_flag) %>%
+  select(Perm_Exits_EnrollmentID, return_flag) %>%
   distinct()
 
 rm(list = ls()[!(ls() %in% c("return_flags"))])
