1c3892d14bf7acf3d23d959fdc452ca420ecb9d2
Genelle Denzin <genelledenzin@cohhio.org>
2021-01-08 16:35:24 -0500

    run same thing every day to check these issues

---
+++ lsa checks.R
@@ -0,0 +1,29 @@
+source("04_DataQuality.R")
+
+a <- dq_main %>%
+  filter(served_between(., "10012018", "09302020") &
+           ProjectType %in% c(1, 2, 3, 8, 9, 13) &
+           Issue %in% c(
+             "Children Only Household",
+             "Too Many Heads of Household",
+             "No Head of Household"
+           ))
+write_csv(a, "hhs.csv")
+
+a <- dq_main %>%
+  filter(served_between(., "10012018", "09302020") &
+           ProjectType %in% c(1, 2, 3, 8, 9, 13) &
+           Issue %in% c(
+             "Missing Client Location"
+           ))
+write_csv(a, "coclocation.csv")
+
+a <- dq_main %>%
+  filter(served_between(., "10012018", "09302020") &
+           ProjectType %in% c(1, 2, 3, 8, 9, 13) &
+           Issue %in% c(
+             "Overlapping Project Stays"
+           ))
+write_csv(a, "overlaps.csv")
+
+
