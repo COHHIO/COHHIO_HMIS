540f685a689947fb452eda295c1d2e91908c3fc1
Genelle Denzin <genelledenzin@cohhio.org>
2021-01-25 17:20:40 -0500

    started working on teasing out which shelters had decreased utilization after April

---
+++ Reports/decreased_utilization_covid.R
@@ -0,0 +1,20 @@
+# COHHIO_HMIS
+# Copyright (C) 2021  Coalition on Homelessness and Housing in Ohio (COHHIO)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details at
+# <https://www.gnu.org/licenses/>.
+
+source("01_Bed_Unit_Utilization.R")
+
+shelters <- utilization_bed %>% filter(ProjectType == 1) %>%
+  rename_all()
+  mutate(pre = mean(01012019:03012020))
+
