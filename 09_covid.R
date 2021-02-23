f423566b31927ebc1fca39038790363ad89b318e
Genelle Denzin <genelledenzin@cohhio.org>
2021-02-23 17:04:09 -0500

    simplified CurrentLocation logic, commented out Concerns piece since we're not going to use it

--- 09_covid.R
+++ 09_covid.R
@@ -15,21 +15,76 @@
 library(tidyverse)
 library(lubridate)
 library(wordcloud)
-library(RColorBrewer)
-library(tm)
+library(leaflet)
+# library(RColorBrewer)
+# library(tm)
+# library(urbnmapr)
+# library(choroplethr)
+# library(choroplethrMaps)
 
-if (!exists("Enrollment")) load("images/COHHIOHMIS.RData")
+if (!exists("Enrollment"))
+  load("images/COHHIOHMIS.RData")
 if (!exists("tay")) {
   load("images/cohorts.RData")
   rlang::env_binding_lock(environment(), ls())
 }
 
+most_recent_entries <- co_clients_served %>%
+  left_join(Enrollment[c("EnrollmentID", "CountyServed")], by = "EnrollmentID") %>%
+  group_by(PersonalID) %>%
+  slice_max(EntryDate) %>%
+  slice_max(EnrollmentID)
+# 
+# counties <- get_urbn_map("counties") %>% 
+#   filter(state_abbv == "OH") %>%
+#   mutate(county_fips = as.numeric(county_fips)) %>%
+#   select(county_name, county_fips) %>% unique()
+
 # Pinpointing where Vaccines are Wanted -----------------------------------
 
-# add a way to more closely pinpoint where these clients are
-vaccine_distribution <- covid19 %>%
+vaccine_distribution_county <- covid19 %>%
   filter(ConsentToVaccine == "Yes (HUD)") %>%
-  select(PersonalID, CountyServed)
+  select(PersonalID, CountyServed) %>%
+  mutate(
+    county_name = paste(CountyServed, "County")
+  ) %>%
+  left_join(counties, by = "county_name") %>%
+  count(county_fips, county_name) %>%
+  select("region" = county_fips, "value" = n)
+
+
+
+# county_choropleth(vaccine_distribution_county,
+#                   state_zoom = "ohio")
+
+vaccine_distribution_provider <- covid19 %>%
+  filter(ConsentToVaccine == "Yes (HUD)") %>%
+  select(PersonalID, CountyServed) %>%
+  left_join(most_recent_entries[c("PersonalID",
+                                  "EntryDate",
+                                  "ExitDate",
+                                  "ProjectName",
+                                  "Destination")], by = "PersonalID") %>%
+  mutate(
+    CurrentLocation = case_when(
+      is.na(EntryDate) ~ "Not currently enrolled in any homeless dedicated project.",
+      today() >= ymd(EntryDate) &
+        (ymd(ExitDate) > today()) |
+        is.na(ExitDate) ~ paste(
+          "Currently in",
+          ProjectName),
+      ymd(ExitDate) <= today() ~ paste(
+        "Exited",
+        ProjectName,
+        "on",
+        ExitDate,
+        "to",
+        living_situation(Destination))
+    )
+  ) %>%
+  count(CurrentLocation)
+
+
 
 # Connecting Clients to their 2nd Doses -----------------------------------
 
@@ -37,23 +92,17 @@ dose_counts <- doses %>%
   count(PersonalID) %>%
   dplyr::rename("DoseCount" = n)
 
-most_recent_entries <- co_clients_served %>%
-  left_join(Enrollment[c("EnrollmentID", "CountyServed")], by = "EnrollmentID") %>%
-  group_by(PersonalID) %>%
-  slice_max(EntryDate) %>%
-  slice_max(EnrollmentID)
-
 vaccine_needs_second_dose <- dose_counts %>%
   filter(DoseCount == 1) %>%
   left_join(doses, by = "PersonalID") %>%
   left_join(most_recent_entries, by = "PersonalID") %>%
   mutate(
     NextDoseNeededDate = case_when(
-      COVID19VaccineManufacturer == "Moderna" ~ 
+      COVID19VaccineManufacturer == "Moderna" ~
         ymd(COVID19DoseDate) + days(28),
-      COVID19VaccineManufacturer == "Pfizer" ~ 
+      COVID19VaccineManufacturer == "Pfizer" ~
         ymd(COVID19DoseDate) + days(21),
-      str_starts(COVID19VaccineManufacturer, "Client doesn't know") == TRUE ~ 
+      str_starts(COVID19VaccineManufacturer, "Client doesn't know") == TRUE ~
         ymd(COVID19DoseDate) + days(28)
     ),
     CurrentLocation = case_when(
@@ -62,35 +111,31 @@ vaccine_needs_second_dose <- dose_counts %>%
         "No contact info and not currently enrolled in any project.",
         VaccineContactInfo
       ),
-      (((
-        ProjectType %in% ph_project_types &
-          today() >= ymd(MoveInDateAdjust)
-      ) |
-        (ProjectType %in% lh_project_types &
-           today() >= ymd(EntryDate))
-      ) &
-        ymd(ExitDate) > today()) |
-        is.na(ExitDate) ~ paste(
+      today() >= ymd(EntryDate) &
+        (ymd(ExitDate) > today()) | is.na(ExitDate) ~ 
+        paste(
           "Currently in",
           ProjectName,
           "Contact Info:",
           VaccineContactInfo
         ),
-      ExitDate <= today() ~ paste(
-        "Exited",
-        ProjectName,
-        "on",
-        ExitDate,
-        "to",
-        living_situation(Destination),
-        "Contact info:",
-        VaccineContactInfo
-      )
+        ExitDate <= today() ~ paste(
+          "Exited",
+          ProjectName,
+          "on",
+          ExitDate,
+          "to",
+          living_situation(Destination),
+          "Contact info:",
+          VaccineContactInfo
+        )
     ),
     DaysUntilNextDose = difftime(ymd(NextDoseNeededDate), today()),
-    VeteranStatus = case_when(VeteranStatus == 0 ~ "No",
-                              VeteranStatus == 1 ~ "Yes",
-                              TRUE ~ "Unknown"),
+    VeteranStatus = case_when(
+      VeteranStatus == 0 ~ "No",
+      VeteranStatus == 1 ~ "Yes",
+      TRUE ~ "Unknown"
+    ),
     AgeAtEntry = case_when(
       AgeAtEntry < 12 ~ "0-11",
       AgeAtEntry < 16 ~ "12-15",
@@ -100,11 +145,13 @@ vaccine_needs_second_dose <- dose_counts %>%
       AgeAtEntry < 85 ~ "75-84",
       AgeAtEntry < 120 ~ "85+",
       TRUE ~ "Unknown"
-    ), 
-    HowSoon = case_when(DaysUntilNextDose < 0 ~ "Overdue",
-                        DaysUntilNextDose < 4 ~ "3 days",
-                        DaysUntilNextDose < 8 ~ "7 days",
-                        DaysUntilNextDose < 29 ~ "Next week")
+    ),
+    HowSoon = case_when(
+      DaysUntilNextDose < 0 ~ "Overdue",
+      DaysUntilNextDose < 4 ~ "3 days",
+      DaysUntilNextDose < 8 ~ "7 days",
+      DaysUntilNextDose < 29 ~ "Next week"
+    )
   ) %>%
   select(
     PersonalID,
@@ -118,75 +165,77 @@ vaccine_needs_second_dose <- dose_counts %>%
     CurrentLocation
   )
 
-# Concerns ----------------------------------------------------------------
+ # Concerns ----------------------------------------------------------------
 
-concerns <- covid19 %>%
-  select(PersonalID, ConsentToVaccine, VaccineConcerns) %>%
-  filter(ConsentToVaccine != "Yes (HUD)" & !is.na(VaccineConcerns))
+# concerns <- covid19 %>%
+#   select(PersonalID, ConsentToVaccine, VaccineConcerns) %>%
+#   filter(ConsentToVaccine != "Yes (HUD)" & !is.na(VaccineConcerns))
+# 
+# text <- concerns$VaccineConcerns
+# 
+# text <- tolower(text)
+# 
+# text <- str_replace(text, "side affects", "side effects")
+# 
+# text <-
+#   removeWords(
+#     text,
+#     c(
+#       "am",
+#       "is",
+#       "are",
+#       "was",
+#       "been",
+#       "did",
+#       "want",
+#       "will",
+#       "would",
+#       "doesnt",
+#       "dont",
+#       "have",
+#       "has",
+#       "hasn't",
+#       "hasnt",
+#       "does",
+#       "doesn't",
+#       "don't",
+#       "the",
+#       "not",
+#       "and",
+#       "vaccine",
+#       "vaccines",
+#       "about",
+#       "into",
+#       "for",
+#       "its",
+#       "it's",
+#       "that"
+#     )
+#   )
+# 
+# cloud <- Corpus(VectorSource(text))
+# 
+# cloud <- tm_map(cloud, content_transformer(tolower)) %>%
+#   tm_map(removeNumbers) %>%
+#   tm_map(removePunctuation) %>%
+#   tm_map(stripWhitespace)
+# 
+# vaccine_concerns_cloud <- wordcloud(
+#   cloud,
+#   colors = brewer.pal(8, "Dark2"),
+#   random.order = FALSE,
+#   random.color = FALSE,
+#   scale = c(4, .2),
+#   fixed.asp = TRUE
+# )
 
-text <- concerns$VaccineConcerns
-
-text <- tolower(text)
-
-text <- str_replace(text, "side affects", "side effects")
-
-text <-
-  removeWords(
-    text,
-    c(
-      "am",
-      "is",
-      "are",
-      "was",
-      "been",
-      "did",
-      "want",
-      "will",
-      "would",
-      "doesnt",
-      "dont",
-      "have",
-      "has",
-      "hasn't",
-      "hasnt",
-      "does",
-      "doesn't",
-      "don't",
-      "the",
-      "not",
-      "and",
-      "vaccine",
-      "vaccines",
-      "about",
-      "into",
-      "for",
-      "its",
-      "it's",
-      "that"
-    )
+rm(list = ls()[!(
+  ls() %in% c(
+    "vaccine_concerns_cloud",
+    "vaccine_needs_second_dose",
+    "vaccine_distribution"
   )
-
-cloud <- Corpus(VectorSource(text))
-
-cloud <- tm_map(cloud, content_transformer(tolower)) %>%
-  tm_map(removeNumbers) %>%
-  tm_map(removePunctuation) %>%
-  tm_map(stripWhitespace)
-
-vaccine_concerns_cloud <- wordcloud(
-  cloud,
-  colors = brewer.pal(8, "Dark2"),
-  random.order = FALSE,
-  random.color = FALSE,
-  scale = c(4, .2),
-  fixed.asp = TRUE
-)
-
-rm(list = ls()[!(ls() %in% c(
-  "vaccine_concerns_cloud",
-  "vaccine_needs_second_dose",
-  "vaccine_distribution"
-))])
+)])
 
 save(list = ls(),
      file = "images/COVID_vaccine.RData",
