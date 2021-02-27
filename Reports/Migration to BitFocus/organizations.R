a7d7d050dbdad357e94608b5d96623cf0bce3eb4
Genelle Denzin <genelledenzin@cohhio.org>
2021-02-27 08:16:34 -0500

    trying to find # of orgs coming over and which of those are "shell" and "not-shell"

--- Reports/Migration to BitFocus/organizations.R
+++ Reports/Migration to BitFocus/organizations.R
@@ -15,17 +15,59 @@
 library(tidyverse)
 library(lubridate)
 library(here)
+library(readxl)
 
-load("images/COHHIOHMIS.RData")
+art_data <- read_xlsx(here("random_data/OrganizationsBitFocus.xlsx"), 
+                      sheet = 4) %>%
+  filter(!is.na(ProjectID)) %>% # dropping deleted providers
+  mutate(MinEntry = as.Date(MinEntry, origin = "1899-12-30"),
+         MaxExit = as.Date(MaxExit, origin = "1899-12-30"),
+         OpenEEs = replace_na(OpenEEs, 0),
+         operating = if_else(operating == "Yes", 1, 0),
+         participating = if_else(participating == "Yes", 1, 0))
 
-participating_providers <- Project %>%
-  filter(HMISParticipatingProject == 1)
+# the warnings are ok
+org_level <- art_data %>%
+  group_by(Org) %>%
+  summarise(minEntry = min(ymd(MinEntry), na.rm = TRUE),
+            maxExit = max(ymd(MaxExit), na.rm = TRUE),
+            openEEs = sum(OpenEEs),
+            maxOperating = max(operating),
+            maxParticipating = max(participating))
 
-participating_orgs <- participating_providers %>%
-  group_by(OrganizationID, OrganizationName) %>%
-  summarise(ParticipatingOrgs = n())
+orgs_data_7yrs <- org_level %>%
+  filter(ymd(maxExit) > ymd("20140501")) %>%
+  select(Org) %>%
+  unique()
 
-orgs <- Organization %>%
-  select(OrganizationID, OrganizationName)
+orgs_data_7yrs_participating <- orgs_data_7yrs %>%
+  left_join(org_level, by = "Org") %>%
+  filter(maxParticipating == 1)
 
+orgs_data_7yrs_not_participating <- orgs_data_7yrs %>%
+  left_join(org_level, by = "Org") %>%
+  filter(maxParticipating == 0)
 
+orgs_data_old <- org_level %>%
+  filter(ymd(maxExit) <= ymd("20140501")) %>%
+  select(Org) %>%
+  unique() 
+
+VASH_only_orgs <- art_data %>%
+  mutate(VASH = if_else(str_detect(Project, "VASH"), 1, 0)) %>%
+  group_by(Org) %>%
+  summarise(total = n(),
+            vash = sum(VASH)) %>%
+  ungroup() %>%
+  filter(total - vash == 0) %>%
+  select(Org)
+
+non_participating_orgs <- art_data %>%
+  anti_join(orgs_data_7yrs, by = "Org") %>%
+  filter(str_starts(Project, "zz", negate = TRUE) &
+           participating == "No") %>%
+  # select(Org) %>%
+  unique()
+
+org_same_as_project <- art_data %>%
+  filter(Org == Project)
