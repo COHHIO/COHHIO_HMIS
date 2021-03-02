01b99ae858fbf952bfcfd6160e6b695b6ad6b06b
Genelle Denzin <genelledenzin@cohhio.org>
2021-03-02 17:04:11 -0500

    got pretty CoC maps finally

--- Reports/COVID19_related/scratch_covid_maps.R
+++ Reports/COVID19_related/scratch_covid_maps.R
@@ -7,11 +7,32 @@ library(leaflet) # for interactive maps
 library(ggplot2) # tidyverse data visualization package
 
 oh_bos_507 <- st_read("Ohio/OH_507/OH_507.shp")
+oh_mah_504 <- st_read("Ohio/OH_504/OH_504.shp")
 ohio_counties <- st_read("Ohio/REFER_COUNTY.shp")
 
-tm_shape(oh_bos_507) + 
-  tm_fill() + 
-  
-  tm_shape(ohio_counties) + 
-  tm_polygons()
-  
+tm_shape(ohio_counties) + 
+  tm_fill(col = "gray") +
+  tm_borders() +
+  tm_shape(oh_bos_507) +
+  tm_fill(col = "#00264d",
+          alpha = .8) +
+  tm_layout(
+    title = "Ohio Balance of State CoC",
+    title.position = c("center", "top"),
+    inner.margins = .08,
+    frame = FALSE
+  )
+
+tm_shape(ohio_counties) + 
+  tm_fill(col = "gray") +
+  tm_borders() +
+  tm_shape(oh_mah_504,
+           alpha = .8) +
+  tm_fill(col = "#00264d") +
+  tm_layout(
+    title = "Mahoning County CoC",
+    title.position = c("center", "top"),
+    inner.margins = .08,
+    frame = FALSE
+  )
+
