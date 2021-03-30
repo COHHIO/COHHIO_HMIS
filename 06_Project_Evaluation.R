5a772977cef317a9b3e75b1946009fa877bfdcfb
Genelle Denzin <genelledenzin@cohhio.org>
2021-03-30 15:16:57 -0400

    removed all my markers that I had checked

--- 06_Project_Evaluation.R
+++ 06_Project_Evaluation.R
@@ -33,30 +33,30 @@ if (!exists("tay")) {
 
 pe_score <- function(structure, value) {
   case_when(
-    structure == "34_40_6" & value >= .4 ~ 6, # GD
-    structure == "34_40_6" & value >= .37 & value < .4 ~ 4, # GD
-    structure == "34_40_6" & value >= .34 & value < .37 ~ 2, # GD
-    structure == "34_40_6" & value < .34 ~ 0, # GD
-    structure == "65_75_10" & value >= .75 ~ 12, # GD
-    structure == "65_75_10" & value >= .7 & value < .75 ~ 9, # GD
-    structure == "65_75_10" & value >= .65 & value < .7 ~ 5, # GD
-    structure == "65_75_10" & value < .65 ~ 0, # GD
+    structure == "34_40_6" & value >= .4 ~ 6, 
+    structure == "34_40_6" & value >= .37 & value < .4 ~ 4, 
+    structure == "34_40_6" & value >= .34 & value < .37 ~ 2, 
+    structure == "34_40_6" & value < .34 ~ 0, 
+    structure == "65_75_10" & value >= .75 ~ 12, 
+    structure == "65_75_10" & value >= .7 & value < .75 ~ 9, 
+    structure == "65_75_10" & value >= .65 & value < .7 ~ 5, 
+    structure == "65_75_10" & value < .65 ~ 0, 
     # structure == "80_90_12" & value >= .9 ~ 12,
     # structure == "80_90_12" & value >= .85 & value < .9 ~ 9,
     # structure == "80_90_12" & value >= .8 & value < .85 ~ 5,
     # structure == "80_90_12" & value < .8 ~ 0,
-    structure == "0_730_10" & value <= 730 ~ 10, # GD
-    structure == "0_730_10" & value > 730 ~ 0, # GD
-    structure == "75_85_10" & value >= .85 ~ 10, # GD
-    structure == "75_85_10" & value >= .8 & value < .85 ~ 7.5, # GD
-    structure == "75_85_10" & value >= .75 & value < .8 ~ 5, # GD
-    structure == "75_85_10" & value < .75 ~ 0, # GD
-    structure == "20_90_10" & value >= .9 ~ 10, # GD
-    structure == "20_90_10" & value >= .75 & value < .9 ~ 8, # GD
-    structure == "20_90_10" & value >= .5 & value < .75 ~ 6, # GD
-    structure == "20_90_10" & value >= .3 & value < .5 ~ 4, # GD
-    structure == "20_90_10" & value >= .2 & value < .3 ~ 2, # GD
-    structure == "20_90_10" & value < .2 ~ 0, # GD
+    structure == "0_730_10" & value <= 730 ~ 10, 
+    structure == "0_730_10" & value > 730 ~ 0, 
+    structure == "75_85_10" & value >= .85 ~ 10, 
+    structure == "75_85_10" & value >= .8 & value < .85 ~ 7.5, 
+    structure == "75_85_10" & value >= .75 & value < .8 ~ 5, 
+    structure == "75_85_10" & value < .75 ~ 0, 
+    structure == "20_90_10" & value >= .9 ~ 10, 
+    structure == "20_90_10" & value >= .75 & value < .9 ~ 8, 
+    structure == "20_90_10" & value >= .5 & value < .75 ~ 6, 
+    structure == "20_90_10" & value >= .3 & value < .5 ~ 4, 
+    structure == "20_90_10" & value >= .2 & value < .3 ~ 2, 
+    structure == "20_90_10" & value < .2 ~ 0, 
     # structure == "2_6_10" & value <= .02 ~ 10,
     # structure == "2_6_10" & value <= .04 & value > .02 ~ 7.5,
     # structure == "2_6_10" & value <= .06 & value > .04 ~ 5,
@@ -85,25 +85,25 @@ pe_score <- function(structure, value) {
     # structure == "22_28_10" & value >= .26 & value < .28 ~ 7.5,
     # structure == "22_28_10" & value >= .22 & value < .26 ~ 5,
     # structure == "22_28_10" & value < .22 ~ 0,
-    structure == "0_7_10_PSH" & value >= 6 & value <= 7 ~ 10, # GD
-    structure == "0_7_10_PSH" & value >= 5 & value < 6 ~ 9, # GD
-    structure == "0_7_10_PSH" & value >= 3 & value < 5 ~ 8, # GD
-    structure == "0_7_10_PSH" & value >= 2 & value < 3 ~ 5, # GD
-    structure == "0_7_10_PSH" & value >= 1 & value < 2 ~ 2, # GD
-    structure == "0_7_10_PSH" & value < 1 ~ 0, # GD
-    structure == "200_280_10" & value <= 200 ~ 10, # GD
-    structure == "200_280_10" & value <= 240 & value > 200 ~ 7.5, # GD
-    structure == "200_280_10" & value <= 280 & value > 240 ~ 5, # GD
-    structure == "200_280_10" & value > 280 ~ 0, # GD
-    structure == "67_75_10" & value >= .75 ~ 10, # GD
-    structure == "67_75_10" & value >= .71 & value < .75 ~ 7.5, # GD
-    structure == "67_75_10" & value >= .67 & value < .71 ~ 5, # GD
-    structure == "67_75_10" & value < .67 ~ 0, # GD
-    structure == "0_7_10" & value >= 4 & value <= 7 ~ 10, # GD
-    structure == "0_7_10" & value >= 3 & value < 4 ~ 8, # GD
-    structure == "0_7_10" & value >= 2 & value < 3 ~ 7, # GD
-    structure == "0_7_10" & value >= 1 & value < 2 ~ 5, # GD
-    structure == "0_7_10" & value < 1 ~ 0, # GD
+    structure == "0_7_10_PSH" & value >= 6 & value <= 7 ~ 10, 
+    structure == "0_7_10_PSH" & value >= 5 & value < 6 ~ 9, 
+    structure == "0_7_10_PSH" & value >= 3 & value < 5 ~ 8, 
+    structure == "0_7_10_PSH" & value >= 2 & value < 3 ~ 5, 
+    structure == "0_7_10_PSH" & value >= 1 & value < 2 ~ 2, 
+    structure == "0_7_10_PSH" & value < 1 ~ 0, 
+    structure == "200_280_10" & value <= 200 ~ 10, 
+    structure == "200_280_10" & value <= 240 & value > 200 ~ 7.5, 
+    structure == "200_280_10" & value <= 280 & value > 240 ~ 5, 
+    structure == "200_280_10" & value > 280 ~ 0, 
+    structure == "67_75_10" & value >= .75 ~ 10, 
+    structure == "67_75_10" & value >= .71 & value < .75 ~ 7.5, 
+    structure == "67_75_10" & value >= .67 & value < .71 ~ 5, 
+    structure == "67_75_10" & value < .67 ~ 0, 
+    structure == "0_7_10" & value >= 4 & value <= 7 ~ 10, 
+    structure == "0_7_10" & value >= 3 & value < 4 ~ 8, 
+    structure == "0_7_10" & value >= 2 & value < 3 ~ 7, 
+    structure == "0_7_10" & value >= 1 & value < 2 ~ 5, 
+    structure == "0_7_10" & value < 1 ~ 0, 
     # structure == "15_19_10" & value <= .15 ~ 10,
     # structure == "15_19_10" & value <= .17 & value > .15 ~ 7.5,
     # structure == "15_19_10" & value <= .19 & value > .17 ~ 5,
@@ -130,21 +130,21 @@ pe_score <- function(structure, value) {
     # structure == "150_210_10" & value <= 170 & value > 150 ~ 7.5,
     # structure == "150_210_10" & value <= 210 & value > 170 ~ 5,
     # structure == "150_210_10" & value > 210 ~ 0,
-    structure == "80_90_10" & value >= .9 ~ 10, # GD
-    structure == "80_90_10" & value >= .85 & value < .9 ~ 7.5, # GD
-    structure == "80_90_10" & value >= .8 & value < .85 ~ 5, # GD
-    structure == "80_90_10" & value < .8 ~ 0, # GD
-    structure == "34_40_10" & value >= .4 ~ 10, # GD
-    structure == "34_40_10" & value >= .37 & value < .4 ~ 7.5, # GD
-    structure == "34_40_10" & value >= .34 & value < .37 ~ 5, # GD
-    structure == "34_40_10" & value < .34 ~ 0, # GD
-    structure == "24_30_10" & value >= .3 ~ 10, # GD
-    structure == "24_30_10" & value >= .27 & value < .3 ~ 7.5, # GD
-    structure == "24_30_10" & value >= .24 & value < .27 ~ 5, # GD
-    structure == "24_30_10" & value < .24 ~ 0, # GD
-    structure == "90_100_5" & value == 1 ~ 5, # GD
-    structure == "90_100_5" & value >= .9 & value < 1 ~ 2, # GD
-    structure == "90_100_5" & value < .9 ~ 0 # GD
+    structure == "80_90_10" & value >= .9 ~ 10, 
+    structure == "80_90_10" & value >= .85 & value < .9 ~ 7.5, 
+    structure == "80_90_10" & value >= .8 & value < .85 ~ 5, 
+    structure == "80_90_10" & value < .8 ~ 0, 
+    structure == "34_40_10" & value >= .4 ~ 10, 
+    structure == "34_40_10" & value >= .37 & value < .4 ~ 7.5, 
+    structure == "34_40_10" & value >= .34 & value < .37 ~ 5, 
+    structure == "34_40_10" & value < .34 ~ 0, 
+    structure == "24_30_10" & value >= .3 ~ 10, 
+    structure == "24_30_10" & value >= .27 & value < .3 ~ 7.5, 
+    structure == "24_30_10" & value >= .24 & value < .27 ~ 5, 
+    structure == "24_30_10" & value < .24 ~ 0, 
+    structure == "90_100_5" & value == 1 ~ 5, 
+    structure == "90_100_5" & value >= .9 & value < 1 ~ 2, 
+    structure == "90_100_5" & value < .9 ~ 0 
   )
 }
 
